efe4cd9c68d97fd2c44f0d37a890647bc965e5da
Gabriel Fontes <eu@misterio.me>
2021-08-10 19:24:07 -0300

    downgrade ethash
    https://github.com/NixOS/nixpkgs/issues/132941

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,8 +3,8 @@
 {
   nixpkgs.overlays = [
     (final: prev: {
-      alacritty-reload = pkgs.alacritty.overrideAttrs (oldAttrs: rec {
-        src = pkgs.fetchFromGitHub {
+      alacritty-reload = prev.alacritty.overrideAttrs (oldAttrs: rec {
+        src = prev.fetchFromGitHub {
           owner = "ncfavier";
           repo = "alacritty";
           rev = "5f392c2cb516a5ea198ebb48754c7c42157d21b3";
@@ -14,6 +14,16 @@
           inherit src;
           outputHash = "sha256-jCNkdgSzoiOW+jh/q3jR9SsiVa/MC5iz6nXgXOqQhdc=";
         });
+      });
+    })
+    (final: prev: {
+      ethash = prev.ethash.overrideAttrs (oldAttrs: rec {
+        src = prev.fetchFromGitHub {
+          owner = "chfast";
+          repo = "ethash";
+          rev = "v0.6.0";
+          sha256 = "sha256-N30v9OZwTmDbltPPmeSa0uOGJhos1VzyS5zY9vVCWfA=";
+        };
       });
     })
   ];
