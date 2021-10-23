329f0548d393b22749754043109fa572896d125f
Gabriel Fontes <eu@misterio.me>
2021-10-23 14:21:06 -0300

    add factorio folder to persist

--- users/misterio/steam.nix
+++ users/misterio/steam.nix
@@ -6,11 +6,13 @@
       allowOther = true;
       directories = [
         "Games/Steam"
+        ".local/share/Steam"
+
+        ".factorio"
         ".config/Hero_Siege"
         ".config/unity3d/Berserk Games/Tabletop Simulator"
         ".config/unity3d/IronGate/Valheim"
         ".local/share/Tabletop Simulator"
-        ".local/share/Steam"
       ];
       files = [ ".steam/steam.token" ".steam/registry.vdf" ];
     };
