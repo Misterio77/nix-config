1322f49f40ddd90245e6c13c8c11fc9db09c36f8
Gabriel Fontes <eu@misterio.me>
2021-11-25 10:54:11 -0300

    remove lutris desktop entries

--- users/misterio/features/games/lutris.nix
+++ users/misterio/features/games/lutris.nix
@@ -1,23 +1,6 @@
 { pkgs, lib, features, ... }: {
   home.packages = [ pkgs.lutris ];
 
-  xdg.desktopEntries = {
-    star-citizen = {
-      name = "Star Citizen";
-      icon = "stargus";
-      exec = "lutris lutris:rungameid/1";
-      categories = [ "Game" ];
-      type = "Application";
-    };
-    league-of-legends = {
-      name = "League of Legends";
-      icon = "league-of-legends";
-      exec = "lutris lutris:rungameid/2";
-      categories = [ "Game" ];
-      type = "Application";
-    };
-  };
-
   home.persistence = lib.mkIf (builtins.elem "persistence" features) {
     "/data/games/misterio" = {
       allowOther = true;
