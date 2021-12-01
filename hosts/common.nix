522ec5ea756db614a3a79e225af644b938a9962b
Gabriel Fontes <eu@misterio.me>
2021-12-01 16:03:28 -0300

    hide blinking cursor

--- hosts/common.nix
+++ hosts/common.nix
@@ -30,7 +30,7 @@
 
   boot = {
     # Quieter boot
-    kernelParams = [ "quiet" "udev.log_priority=3" ];
+    kernelParams = [ "quiet" "udev.log_priority=3" "vt.global_cursor_default=0" ];
     consoleLogLevel = 0;
     initrd.verbose = false;
   };
