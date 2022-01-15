dae04165672fffa28362b70f63ae01160c6c01ee
Gabriel Fontes <eu@misterio.me>
2022-01-15 11:47:39 -0300

    update nix colors

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     declarative-cachix.url = "github:jonascarpay/declarative-cachix";
     impermanence.url = "github:RiscadoA/impermanence";
 
-    nix-colors.url = "github:Misterio77/nix-colors/add-vim-scheme";
+    nix-colors.url = "github:Misterio77/nix-colors";
 
     utils.url = "github:numtide/flake-utils";
 
