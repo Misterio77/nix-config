c6219aae6b203bd3f4aaeb0301781208bd1e959d
Gabriel Fontes <eu@misterio.me>
2022-01-24 00:28:40 -0300

    run deluge in non declarative mode

--- hosts/merope/deluge.nix
+++ hosts/merope/deluge.nix
@@ -1,18 +1,14 @@
 { config, ... }: {
   services.deluge = {
     enable = true;
-    declarative = true;
-    authFile = /data/srv/deluge.key;
-    config = {
-      download_location = "/srv/torrents/";
-      allow_remote = true;
-      daemon_port = 58846;
-      listen_ports = [ 6881 6889 ];
-    };
-    openFirewall = true;
   };
+
   networking.firewall = {
-    allowedTCPPorts = [ config.services.deluge.config.daemon_port ];
-    allowedUDPPorts = [ config.services.deluge.config.daemon_port ];
+    allowedTCPPorts = [ 58846 ];
+    allowedUDPPorts = [ 58846 ];
+  };
+
+  environment.persistence."/data" = {
+    directories = [ "/var/lib/deluge" ];
   };
 }
