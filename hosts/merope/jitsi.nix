e57ca9ce258a221ab003dc3f947a95780f2e480b
Gabriel Fontes <eu@misterio.me>
2022-02-04 20:46:57 -0300

    merope: customize jitsi a bit

--- hosts/merope/jitsi.nix
+++ hosts/merope/jitsi.nix
@@ -4,7 +4,14 @@
     jitsi-meet = {
       enable = true;
       hostName = "jitsi.misterio.me";
+      interfaceConfig = {
+        APP_NAME = "Meet do Gabiru";
+        SHOW_JITSI_WATERMARK = false;
+        DISPLAY_WELCOME_FOOTER = false;
+      };
       nginx.enable = true;
+      # Can't enable this until chromedriver is packaged for aarch64
+      # jibri.enable = true;
     };
   };
 
