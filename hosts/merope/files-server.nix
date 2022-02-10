d88df0c55f3bbd3ecfd4066b7b7ade980f6084e2
Gabriel Fontes <eu@misterio.me>
2022-02-10 13:21:44 -0300

    merope: tidy up file server

--- hosts/merope/files-server.nix
+++ hosts/merope/files-server.nix
@@ -1,12 +1,10 @@
 { config, ... }: {
-  services = {
-    nginx.virtualHosts = {
-      "files.misterio.me" = {
-        default = true;
-        forceSSL = true;
-        enableACME = true;
-        locations."/".root = "/media/files";
-      };
+  services.nginx.virtualHosts = {
+    "files.misterio.me" = {
+      default = true;
+      forceSSL = true;
+      enableACME = true;
+      locations."/".root = "/media/files";
     };
   };
 }
