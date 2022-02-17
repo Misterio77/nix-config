bd011bc9b0571dff051519f4750395b52f6a9cb4
Gabriel Fontes <eu@misterio.me>
2022-02-17 06:34:45 -0300

    use btrfs module on maia

--- hosts/maia/hardware-configuration.nix
+++ hosts/maia/hardware-configuration.nix
@@ -1,73 +1,23 @@
-{ config, lib, pkgs, modulesPath, hostname, ... }:
+{ modulesPath, ... }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    ../common/btrfs-optin-persistence.nix
+    ../common/encrypted-root.nix
+  ];
 
   boot = {
     initrd = {
-      availableKernelModules =
-        [ "usbhid" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
-      luks.devices.${hostname}.device = "/dev/disk/by-label/${hostname}";
-      postDeviceCommands = lib.mkBefore ''
-        mkdir -p /mnt
-        mount -o subvol=/ /dev/mapper/${hostname} /mnt
-
-        echo "Cleaning subvolume"
-        btrfs subvolume list -o /mnt/root | cut -f9 -d ' ' |
-        while read subvolume; do
-          btrfs subvolume delete "/mnt/$subvolume"
-        done && btrfs subvolume delete /mnt/root
-
-        echo "Restoring blank subvolume"
-        btrfs subvolume snapshot /mnt/root-blank /mnt/root
-
-        umount /mnt
-      '';
+      availableKernelModules = [ "usbhid" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+      kernelModules = [ "kvm-intel" ];
     };
-    kernelModules = [ "kvm-intel" ];
-    supportedFilesystems = [ "btrfs" ];
   };
 
   fileSystems = {
-    "/" = {
-      device = "/dev/mapper/${hostname}";
-      fsType = "btrfs";
-      options = [ "subvol=root" "compress=zstd" ];
-    };
-
-    "/nix" = {
-      device = "/dev/mapper/${hostname}";
-      fsType = "btrfs";
-      options = [ "subvol=nix" "noatime" "compress=zstd" ];
-    };
-
-    "/persist" = {
-      device = "/dev/mapper/${hostname}";
-      fsType = "btrfs";
-      options = [ "subvol=persist" "compress=zstd" ];
-      neededForBoot = true;
-    };
-
-    "/dotfiles" = {
-      device = "/dev/mapper/${hostname}";
-      fsType = "btrfs";
-      options = [ "subvol=dotfiles" "compress=zstd" ];
-    };
-
-    "/swap" = {
-      device = "/dev/mapper/${hostname}";
-      fsType = "btrfs";
-      options = [ "subvol=swap" "noatime" "compress=lzo" ];
-    };
-
     "/boot" = {
       device = "/dev/disk/by-label/ESP";
       fsType = "vfat";
     };
   };
-
-  swapDevices = [{
-    device = "/swap/swapfile";
-    size = 4096;
-  }];
 }
