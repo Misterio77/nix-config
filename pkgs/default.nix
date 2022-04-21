9016347c03ece492bb4594cbfd7a03ff62055d90
Gabriel Fontes <eu@misterio.me>
2022-04-21 10:07:08 -0300

    remove xiaomitool

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -11,5 +11,4 @@
   shellcolord = pkgs.callPackage ./shellcolord { };
   swayfader = pkgs.callPackage ./swayfader { };
   wallpapers = pkgs.callPackage ./wallpapers { };
-  xiaomitool = pkgs.callPackage ./xiaomitool { };
 }
