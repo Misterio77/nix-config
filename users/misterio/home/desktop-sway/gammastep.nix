9a3749dd1d151b8ece9f3c9fb2c92d5a44ab47d3
Gabriel Fontes <eu@misterio.me>
2022-04-22 11:16:02 -0300

    adjust gammastep color

--- users/misterio/home/desktop-sway/gammastep.nix
+++ users/misterio/home/desktop-sway/gammastep.nix
@@ -2,6 +2,10 @@
   services.gammastep = {
     enable = true;
     provider = "geoclue2";
+    temperature = {
+      day = 6000;
+      night = 4600;
+    };
     settings = {
       general.adjustment-method = "wayland";
     };
