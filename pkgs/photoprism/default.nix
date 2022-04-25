031048a3caf47e69010c31253a7c5e5ed664354d
Gabriel Fontes <eu@misterio.me>
2022-04-25 20:00:21 -0300

    put photoprism tensorflow into same file

--- pkgs/photoprism/default.nix
+++ pkgs/photoprism/default.nix
@@ -1,25 +1,11 @@
 # From https://github.com/newam/nixpkgs
-{ lib
-, pkgs
-, stdenv
-, buildGoModule
-, fetchFromGitHub
-, fetchzip
-, libtensorflow-bin
-, coreutils
-, fetchurl
-, darktable
-, rawtherapee
-, ffmpeg
-, libheif
-, exiftool
-, nodejs
-, system
-}:
+{ lib, pkgs, stdenv, buildGoModule, fetchFromGitHub, fetchzip, coreutils, fetchurl, darktable, rawtherapee, ffmpeg, libheif, exiftool, nodejs }:
 
 let
   version = "unstable-2022-03-02";
   pname = "photoprism";
+
+  inherit (stdenv.hostPlatform) system;
 
   src = fetchFromGitHub {
     owner = pname;
@@ -50,12 +36,90 @@ let
     sha256 = "sha256-zy/HcmgaHOY7FfJUY6I/yjjsMPHR2Ote9ppwqemBlfg=";
   };
 
+  libtensorflow = stdenv.mkDerivation rec {
+    pname = "libtensorflow-photoprism";
+    version = "1.15.2";
+
+    srcs = [
+      # Downstream photoprism libtensorflow tarball (with pre-built libs for both arm64 and amd64)
+      (fetchurl {
+        sha256 = {
+          x86_64-linux = "sha256-bZAC3PJxqcjuGM4RcNtzYtkg3FD3SrO5beDsPoKenzc=";
+          aarch64-linux = "sha256-qnj4vhSWgrk8SIjzIH1/4waMxMsxMUvqdYZPaSaUJRk=";
+        }.system or (throw "Unsupported system");
+
+        url =
+          let
+            systemName = {
+              x86_64-linux = "amd64";
+              aarch64-linux = "arm64";
+            }.system or (throw "Unsupported system");
+          in
+          "https://dl.photoprism.app/tensorflow/${systemName}/libtensorflow-${systemName}-${version}.tar.gz";
+      })
+      # Upstream tensorflow tarball (with includes we'll need)
+      (fetchurl {
+        url = "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-1.15.0.tar.gz";
+        sha256 = "sha256-3sv9WnCeztNSP1XM+iOTN6h+GrPgAO/aNhfbeeEDTe0=";
+      })
+    ];
+
+    sourceRoot = ".";
+
+    # Pierce both sources together
+    unpackPhase = ''
+      sources=($srcs)
+
+      mkdir downstream upstream
+      tar xf ''${sources[0]} --directory downstream
+      tar xf ''${sources[1]} --directory upstream
+
+      mv downstream/lib .
+      mv upstream/{include,LICENSE,THIRD_PARTY_TF_C_LICENSES} .
+      rm -r downstream upstream
+
+      cd lib
+      ln -sT libtensorflow.so{,.1}
+      ln -sT libtensorflow_framework.so{,.1}
+      cd ..
+    '';
+
+    # Patch library to use our libc, libstdc++ and others
+    patchPhase =
+      let
+        rpath = makeLibraryPath [ stdenv.cc.libc stdenv.cc.cc.lib ];
+      in
+      ''
+        chmod -R +w lib
+        patchelf --set-rpath "${rpath}:$out/lib" lib/libtensorflow.so
+        patchelf --set-rpath "${rpath}" lib/libtensorflow_framework.so
+      '';
+
+    buildPhase = ''
+      # Write pkg-config file.
+      mkdir lib/pkgconfig
+      cat > lib/pkgconfig/tensorflow.pc << EOF
+      Name: TensorFlow
+      Version: ${version}
+      Description: Library for computation using data flow graphs for scalable machine learning
+      Requires:
+      Libs: -L$out/lib -ltensorflow
+      Cflags: -I$out/include/tensorflow
+      EOF
+    '';
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r LICENSE THIRD_PARTY_TF_C_LICENSES lib include $out
+    '';
+  };
+
   backend = buildGoModule rec {
     inherit pname version src;
 
     buildInputs = [
       coreutils
-      (pkgs.callPackage ./tensorflow.nix { })
+      libtensorflow
     ];
 
     postPatch = ''
@@ -74,16 +138,13 @@ let
   };
 
   inherit (import ./node-composition.nix {
-    inherit pkgs nodejs;
-    inherit (stdenv.hostPlatform) system;
+    inherit pkgs nodejs system;
   }) nodeDependencies;
 
   frontend = stdenv.mkDerivation {
     name = "photoprism-frontend";
     inherit src;
     buildInputs = [ nodejs ];
-
-    # sourceRoot = "source/frontend";
 
     buildPhase = ''
       runHook preBuild
