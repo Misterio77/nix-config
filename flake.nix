0033cead1d716caed7d5bbaaaa3f824f8721c133
Gabriel Fontes <eu@misterio.me>
2022-07-06 17:50:35 -0300

    lab bd bump

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     projeto-lab-bd = {
-      url = "github:misterio77/SCC0541-Lab-BD-Projeto";
+      url = "github:misterio77/SCC0541-Lab-BD-Projeto/0.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
