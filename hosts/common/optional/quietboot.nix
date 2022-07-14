9bc79977dc86e4dbfe252fa1239a56d6698f2e7d
Gabriel Fontes <eu@misterio.me>
2022-07-14 00:17:20 -0300

    nixos: hide cursor on tty

--- hosts/common/optional/quietboot.nix
+++ hosts/common/optional/quietboot.nix
@@ -16,6 +16,7 @@
       "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
       "udev.log_priority=3"
+      "vt.global_cursor_default=0"
     ];
     consoleLogLevel = 0;
     initrd.verbose = false;
