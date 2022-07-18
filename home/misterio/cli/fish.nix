33749d1519f7a4bdc5c9503f60a6031057811028
Gabriel Fontes <eu@misterio.me>
2022-07-18 00:45:56 -0300

    home/fish: always start kitty integration

--- home/misterio/cli/fish.nix
+++ home/misterio/cli/fish.nix
@@ -48,11 +48,10 @@
       '' +
       # kitty integration
       ''
-        if set -q KITTY_INSTALLATION_DIR
-          set --global KITTY_SHELL_INTEGRATION enabled
-          source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
-          set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
-        end
+        set --global KITTY_INSTALLATION_DIR "${pkgs.kitty}/lib/kitty"
+        set --global KITTY_SHELL_INTEGRATION enabled
+        source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
+        set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
       '' +
       # Use vim bindings and cursors
       ''
