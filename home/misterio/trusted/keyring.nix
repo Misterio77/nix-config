43bb2eba4b0479871c65688c473292d4e9123316
Gabriel Fontes <eu@misterio.me>
2022-07-18 00:46:18 -0300

    home/gpg: only check for unlocked if gpg-agent is running (to avoid starting it)

--- home/misterio/trusted/keyring.nix
+++ home/misterio/trusted/keyring.nix
@@ -4,6 +4,6 @@ let
   ssh = "${pkgs.openssh}/bin/ssh";
   gpg-connect-agent = "${pkgs.gnupg}/bin/gpg-connect-agent";
 in {
-  isUnlocked = "${gpg-connect-agent} 'scd getinfo card_list' /bye | grep SERIALNO -q";
+  isUnlocked = "${pkgs.procps}/bin/pgrep 'gpg-agent' && ${gpg-connect-agent} 'scd getinfo card_list' /bye | grep SERIALNO -q";
   unlock = "${ssh} -T localhost -o StrictHostKeyChecking=no exit";
 }
