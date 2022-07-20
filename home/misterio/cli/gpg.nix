b870c2eb4d671d6cff01168f354a3e5f31c4ac6a
Gabriel Fontes <eu@misterio.me>
2022-07-20 01:56:24 -0300

    home/gpg: start gpg agent correctly

--- home/misterio/cli/gpg.nix
+++ home/misterio/cli/gpg.nix
@@ -11,11 +11,6 @@ let
       package = pkgs.pinentry-curses;
       name = "curses";
     };
-
-  startGpgAgent = ''
-    # Start gpg-agent if it's not running or tunneled in
-    gpg-agent &> /dev/null || eval $(gpg-agent --daemon) &> /dev/null
-  '';
 in
 {
   home.packages = [ pinentry.package ];
@@ -29,9 +24,12 @@ in
   };
 
   programs = {
-    bash.profileExtra = startGpgAgent;
-    fish.loginShellInit = startGpgAgent;
-    zsh.loginExtra = startGpgAgent;
+    # Start gpg-agent if it's not running or tunneled in
+    # SSH does not start it automatically, so this is needed to avoid having to use a gpg command at startup
+    # https://www.gnupg.org/faq/whats-new-in-2.1.html#autostart
+    bash.profileExtra = "gpgconf --launch gpg-agent";
+    fish.loginShellInit = "gpgconf --launch gpg-agent";
+    zsh.loginExtra = "gpgconf --launch gpg-agent";
   };
 
   home.persistence = lib.mkIf persistence {
