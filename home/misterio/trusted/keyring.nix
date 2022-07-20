dbd1365502c188b34afbd93f6aa085283a7fe252
Gabriel Fontes <eu@misterio.me>
2022-07-20 01:48:36 -0300

    home/waybar: fix gpg module output

--- home/misterio/trusted/keyring.nix
+++ home/misterio/trusted/keyring.nix
@@ -4,6 +4,6 @@ let
   ssh = "${pkgs.openssh}/bin/ssh";
   gpg-connect-agent = "${pkgs.gnupg}/bin/gpg-connect-agent";
 in {
-  isUnlocked = "${pkgs.procps}/bin/pgrep 'gpg-agent' && ${gpg-connect-agent} 'scd getinfo card_list' /bye | ${pkgs.gnugrep}/bin/grep SERIALNO -q";
+  isUnlocked = "${pkgs.procps}/bin/pgrep 'gpg-agent' &> /dev/null && ${gpg-connect-agent} 'scd getinfo card_list' /bye | ${pkgs.gnugrep}/bin/grep SERIALNO -q";
   unlock = "${ssh} -T localhost -o StrictHostKeyChecking=no exit";
 }
