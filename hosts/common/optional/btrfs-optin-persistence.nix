bb64deb8a4400da1b71c539786fa7b32948c9b1d
Gabriel Fontes <eu@misterio.me>
2022-08-06 01:25:10 -0300

    hosts: don't wipe btrfs if there's a dontwipe file

--- hosts/common/optional/btrfs-optin-persistence.nix
+++ hosts/common/optional/btrfs-optin-persistence.nix
@@ -4,14 +4,18 @@
       mkdir -p /mnt
       mount -o subvol=/ /dev/disk/by-label/${hostname} /mnt
 
-      echo "Cleaning subvolume"
-      btrfs subvolume list -o /mnt/root | cut -f9 -d ' ' |
-      while read subvolume; do
-        btrfs subvolume delete "/mnt/$subvolume"
-      done && btrfs subvolume delete /mnt/root
+      if [ -e "/mnt/root/dontwipe" ]; then
+        echo "Not wiping root"
+      else
+        echo "Cleaning subvolume"
+        btrfs subvolume list -o /mnt/root | cut -f9 -d ' ' |
+        while read subvolume; do
+          btrfs subvolume delete "/mnt/$subvolume"
+        done && btrfs subvolume delete /mnt/root
 
-      echo "Restoring blank subvolume"
-      btrfs subvolume snapshot /mnt/root-blank /mnt/root
+        echo "Restoring blank subvolume"
+        btrfs subvolume snapshot /mnt/root-blank /mnt/root
+      fi
 
       umount /mnt
     '';
