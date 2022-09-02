0709323d0c0c39ac5e659cdc9f31e24e0b291556
Gabriel Fontes <eu@misterio.me>
2022-09-02 18:15:25 -0300

    home/cli: add bc

--- home/misterio/cli/default.nix
+++ home/misterio/cli/default.nix
@@ -21,6 +21,7 @@
     comma # Install and run programs by sticking a , before them
     distrobox # Nice escape hatch, integrates docker images with my environment
 
+    bc # Calculator
     bottom # System viewer
     ncdu # TUI disk usage
     exa # Better ls
