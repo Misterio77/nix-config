d76438543fe20542292c752faabdfcd0d07d6989
Gabriel Fontes <eu@misterio.me>
2022-09-05 17:24:01 -0300

    home/ssh: readd ssh-only forwarding block

--- home/misterio/cli/ssh.nix
+++ home/misterio/cli/ssh.nix
@@ -6,13 +6,19 @@ in
 {
   programs.ssh = {
     enable = true;
-    matchBlocks.net = {
-      host = builtins.concatStringsSep " " hostnames;
-      forwardAgent = true;
-      remoteForwards = [{
-        bind.address = ''/%d/.gnupg/sockets/S.gpg-agent'';
-        host.address = ''/%d/.gnupg/sockets/S.gpg-agent.extra'';
-      }];
+    matchBlocks = {
+      net = {
+        host = builtins.concatStringsSep " " hostnames;
+        forwardAgent = true;
+        remoteForwards = [{
+          bind.address = ''/%d/.gnupg/sockets/S.gpg-agent'';
+          host.address = ''/%d/.gnupg/sockets/S.gpg-agent.extra'';
+        }];
+      };
+      trusted = lib.hm.dag.entryBefore [ "net" ] {
+        host = "m7.rs *.m7.rs *.ts.m7.rs";
+        forwardAgent = true;
+      };
     };
   };
 
