ed2efa8307ca54cef9e269de800cc493f27b7ffe
Gabriel Fontes <eu@misterio.me>
2022-09-09 22:39:03 -0300

    electra/mail: enable for all my domains

--- hosts/electra/services/mail.nix
+++ hosts/electra/services/mail.nix
@@ -4,18 +4,23 @@
     inputs.nixos-mailserver.nixosModules.mailserver
   ];
 
-  mailserver = {
+  mailserver = rec {
     enable = true;
     fqdn = "mail.m7.rs";
     sendingFqdn = "electra.m7.rs";
-    domains = [ "m7.rs" ];
+    domains = [
+      "m7.rs"
+      "fontes.dev.br"
+      "misterio.me"
+      "gsfontes.com"
+    ];
     useFsLayout = true;
     certificateScheme = 3;
     localDnsResolver = false;
     loginAccounts = {
       "hi@m7.rs" = {
         hashedPasswordFile = config.sops.secrets.gabriel-mail-password.path;
-        aliases = [ "@m7.rs" ];
+        aliases = map (d: "@" + d) domains;
       };
     };
     mailboxes = {
