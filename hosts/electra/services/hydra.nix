40476d0e6f244d143d3c67f6b2bb6714e7b4c73e
Gabriel Fontes <hi@m7.rs>
2022-10-07 15:21:51 -0300

    electra/hydra: add proxy, github integration

--- hosts/electra/services/hydra.nix
+++ hosts/electra/services/hydra.nix
@@ -1,9 +1,35 @@
+{ config, ... }:
 {
-  services.hydra = {
-    enable = true;
-    hydraURL = "https://hydra.m7.rs";
-    listenHost = "localhost";
-    notificationSender = "hydra@m7.rs";
-    useSubstitutes = true;
+  services = {
+    hydra = {
+      enable = true;
+      hydraURL = "https://hydra.m7.rs";
+      notificationSender = "hydra@m7.rs";
+      listenHost = "localhost";
+      smtpHost = "localhost";
+      useSubstitutes = true;
+      extraConfig = /* xml */ ''
+        Include ${config.sops.secrets.hydra-gh-auth.path}
+        <githubstatus>
+          jobs = nix-config:master:*
+        </githubstatus>
+      '';
+    };
+    nginx.virtualHosts = {
+      "hydra.m7.rs" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/".proxyPass =
+          "http://localhost:${toString config.services.hydra.port}";
+      };
+    };
+  };
+  users.users.hydra-queue-runner.extraGroups = [ config.users.users.hydra.group ];
+  sops.secrets = {
+    hydra-gh-auth = {
+      sopsFile = ../secrets.yaml;
+      owner = config.users.users.hydra.name;
+      group = config.users.users.hydra.group;
+    };
   };
 }
