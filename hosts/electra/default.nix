fdfa31046bc60ca6ff03de0029ef99a6f545ecfd
Gabriel Fontes <hi@m7.rs>
2022-10-08 15:16:25 -0300

    electra: also emulate aarch64

--- hosts/electra/default.nix
+++ hosts/electra/default.nix
@@ -11,5 +11,7 @@
   system.stateVersion = "22.05";
   # Slows down write operations considerably
   nix.settings.auto-optimise-store = false;
+
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 }
 
