6ba9bdc03babc72f7b0d974c7df7fcb1ba94cdbb
Gabriel Fontes <hi@m7.rs>
2022-10-11 11:52:53 -0300

    only use distributed builds in electra

--- hosts/electra/services/hydra.nix
+++ hosts/electra/services/hydra.nix
@@ -1,9 +1,42 @@
-{ config, ... }:
-{
-  # https://github.com/NixOS/nix/issues/5039
-  nix.extraOptions = ''
-    allowed-uris = https:// http://
-  '';
+{ config, pkgs, ... }: {
+  nix = {
+    settings.trusted-users = [ config.users.users.builder.name ];
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        hostName = "atlas";
+        systems = [ "x86_64-linux" "aarch64-linux" ];
+
+        sshKey = config.sops.secrets.builder-ssh-key.path;
+        sshUser = config.users.users.builder.name;
+        maxJobs = 12;
+        speedFactor = 100;
+      }
+      {
+        hostName = "maia";
+        systems = [ "x86_64-linux" "aarch64-linux" ];
+
+        sshKey = config.sops.secrets.builder-ssh-key.path;
+        sshUser = config.users.users.builder.name;
+        maxJobs = 8;
+        speedFactor = 60;
+      }
+      {
+        hostName = "local";
+        systems = [ "builtin" "x86_64-linux" "aarch64-linux" ];
+        protocol = null;
+
+        maxJobs = 2;
+        speedFactor = 20;
+      }
+    ];
+
+    # https://github.com/NixOS/nix/issues/5039
+    extraOptions = ''
+      allowed-uris = https:// http://
+    '';
+  };
+
   services = {
     hydra = {
       enable = true;
@@ -29,21 +62,34 @@
       };
     };
   };
-  users.users = {
-    hydra-queue-runner.extraGroups = [
-      config.users.users.hydra.group
-      config.users.users.builder.group
-    ];
-    hydra-www.extraGroups = [
-      config.users.users.hydra.group
-      config.users.users.builder.group
-    ];
+
+  users = {
+    groups = {
+      builder = { };
+    };
+    users = {
+      builder = {
+        isSystemUser = true;
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAz0dIbaTuAihil/si33MQSFH5yBFoupwnV5gcq2CCbO (none)" ];
+        group = "builder";
+        shell = pkgs.bash;
+      };
+      hydra-queue-runner.extraGroups = with config.users.users; [ hydra.group builder.group ];
+      hydra-www.extraGroups = with config.users.users; [ hydra.group builder.group ];
+    };
   };
+
   sops.secrets = {
     hydra-gh-auth = {
       sopsFile = ../secrets.yaml;
       owner = config.users.users.hydra.name;
       group = config.users.users.hydra.group;
+      mode = "0440";
+    };
+    builder-ssh-key = {
+      sopsFile = ../secrets.yaml;
+      owner = config.users.users.builder.name;
+      group = config.users.users.builder.group;
       mode = "0440";
     };
   };
