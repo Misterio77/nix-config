b7ad876d8652aa2c78cc5ab21c82ce7ecf875997
Gabriel Fontes <hi@m7.rs>
2022-10-21 16:00:57 -0300

    home/nvim: group autocmds into augroups

--- home/misterio/features/cli/nvim/default.nix
+++ home/misterio/features/cli/nvim/default.nix
@@ -24,7 +24,10 @@
 
         "Set fold level to highest in file
         "so everything starts out unfolded at just the right level
-        autocmd BufWinEnter * let &foldlevel = max(map(range(1, line('$')), 'foldlevel(v:val)'))
+        augroup initial_fold
+          autocmd!
+          autocmd BufWinEnter * let &foldlevel = max(map(range(1, line('$')), 'foldlevel(v:val)'))
+        augroup END
 
         "Tabs
         set tabstop=4 "4 char-wide tab
@@ -32,17 +35,27 @@
         set softtabstop=0 "Use same length as 'tabstop'
         set shiftwidth=0 "Use same length as 'tabstop'
         "2 char-wide overrides
-        autocmd FileType json,html,htmldjango,hamlet,nix,scss,typescript,php,haskell,terraform setlocal tabstop=2
+        augroup two_space_tab
+          autocmd!
+          autocmd FileType json,html,htmldjango,hamlet,nix,scss,typescript,php,haskell,terraform setlocal tabstop=2
+        augroup END
 
         "Set tera to use htmldjango syntax
-        autocmd BufRead,BufNewFile *.tera setfiletype htmldjango
+        augroup tera_htmldjango
+          autocmd!
+          autocmd BufRead,BufNewFile *.tera setfiletype htmldjango
+        augroup END
 
         "Options when composing mutt mail
-        autocmd FileType mail set noautoindent wrapmargin=0 textwidth=0 linebreak wrap formatoptions +=w
+        augroup mail_settings
+          autocmd FileType mail set noautoindent wrapmargin=0 textwidth=0 linebreak wrap formatoptions +=w
+        augroup END
 
         "Fix nvim size according to terminal
         "(https://github.com/neovim/neovim/issues/11330)
-        autocmd VimEnter * silent exec "!kill -s SIGWINCH" getpid()
+        augroup fix_size
+          autocmd VimEnter * silent exec "!kill -s SIGWINCH" getpid()
+        augroup END
 
         "Line numbers
         set number relativenumber
@@ -202,7 +215,7 @@
           orgmode.setup_ts_grammar()
           orgmode.setup{
             org_agenda_files = '~/Documents/Org/**/*',
-            org_default_notes_file = '~/Documents/Org/capture.org',
+            org_default_notes_file = '~/Documents/Org/todo/capture.org',
           }
         '';
       }
