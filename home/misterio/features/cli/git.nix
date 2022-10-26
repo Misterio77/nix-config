2cc8851b2d847c0f8c8d074f177368ade840e640
Gabriel Fontes <hi@m7.rs>
2022-10-26 05:37:50 -0300

    home/git: add git m7 ls

--- home/misterio/features/cli/git.nix
+++ home/misterio/features/cli/git.nix
@@ -7,9 +7,14 @@ let
     EOF
     git remote add origin git@m7.rs:"$1.git"
   '';
+  git-m7-ls = pkgs.writeShellScriptBin "git-m7-ls" ''
+    ${pkgs.openssh}/bin/ssh git@m7.rs << EOF
+      ls
+    EOF
+  '';
 in
 {
-  home.packages = [ git-m7-init ];
+  home.packages = [ git-m7-init git-m7-ls ];
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull;
