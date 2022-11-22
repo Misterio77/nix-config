ba732a2ce5309021a4d1714dd12d1746e814433f
Gabriel Fontes <hi@m7.rs>
2022-11-21 21:51:06 -0300

    hosts/nix: only use my binary cache
    having many binary caches makes stuff slow, as it queries all of them

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -2,16 +2,8 @@
 {
   nix = {
     settings = {
-      substituters = [
-        "https://cache.m7.rs"
-        "https://hyprland.cachix.org"
-        "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
+      substituters = [ "https://cache.m7.rs" ];
+      trusted-public-keys = [ "cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg=" ];
       trusted-users = [ "root" "@wheel" ];
       auto-optimise-store = lib.mkDefault true;
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
