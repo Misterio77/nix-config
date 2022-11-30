5f11d8b91d671a49d694315c8ec99285bd66f518
Gabriel Fontes <hi@m7.rs>
2022-11-30 09:09:27 -0300

    home/hyprland: disable console output

--- home/misterio/features/desktop/hyprland/default.nix
+++ home/misterio/features/desktop/hyprland/default.nix
@@ -8,17 +8,17 @@
   programs = {
     fish.loginShellInit = ''
       if test (tty) = "/dev/tty1"
-        exec Hyprland
+        exec Hyprland &> /dev/null
       end
     '';
     zsh.loginExtra = ''
       if [ "$(tty)" = "/dev/tty1" ]; then
-        exec Hyprland
+        exec Hyprland &> /dev/null
       fi
     '';
     zsh.profileExtra = ''
       if [ "$(tty)" = "/dev/tty1" ]; then
-        exec Hyprland
+        exec Hyprland &> /dev/null
       fi
     '';
   };
