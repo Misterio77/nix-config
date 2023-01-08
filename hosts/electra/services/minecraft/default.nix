3935717c9fd77908c17d93ab0e49cf6fd531d5ec
Gabriel Fontes <hi@m7.rs>
2023-01-07 23:02:22 -0300

    hosts/minecraft: declarative db

--- hosts/electra/services/minecraft/default.nix
+++ hosts/electra/services/minecraft/default.nix
@@ -1,10 +1,27 @@
 { inputs, pkgs, outputs, config, lib, ... }:
 let
-  lib' = import ./lib.nix { inherit pkgs; };
+  lib' = pkgs.callPackage ./lib.nix { };
   minecraftPkgs = inputs.nix-minecraft.packages.${pkgs.system};
 in
 {
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
+
+  sops.secrets = {
+    # VELOCITY_FORWARDING_SECRET=...
+    # DATABASE_PASSWORD=...
+    minecraft-secrets = {
+      owner = "minecraft";
+      group = "minecraft";
+      sopsFile = ../../secrets.yaml;
+    };
+    # CREATE USER IF NOT EXISTS 'minecraft'@'localhost';
+    # ALTER USER 'minecraft'@'localhost' IDENTIFIED VIA unix_socket OR mysql_native_password USING PASSWORD(...);
+    set-minecraft-sql-password = {
+      owner = "mysql";
+      group = "mysql";
+      sopsFile = ../../secrets.yaml;
+    };
+  };
 
   networking.firewall = {
     allowedTCPPorts = [ 25565 ];
@@ -17,16 +34,9 @@ in
     ensureDatabases = [ "minecraft" ];
     ensureUsers = [{
       name = "minecraft";
-      ensurePermissions = {
-        "minecraft.*" = "ALL PRIVILEGES";
-      };
+      ensurePermissions = { "minecraft.*" = "ALL PRIVILEGES"; };
     }];
-  };
-
-  sops.secrets.minecraft-secrets = {
-    owner = "minecraft";
-    group = "minecraft";
-    sopsFile = ../../secrets.yaml;
+    initialScript = lib.mkAfter config.sops.secrets.set-minecraft-sql-password.path;
   };
 
   services.minecraft-servers = {
@@ -82,6 +92,18 @@ in
             };
             database.storage-type = "sqlite";
           };
+          "plugins/luckperms/config.yml" = lib'.toYAMLFile {
+            server = "proxy";
+            storage-method = "mysql";
+            data = {
+              address = "127.0.0.1";
+              database = "minecraft";
+              username = "minecraft";
+              password = "@DATABASE_PASSWORD@";
+              table-prefix = "luckperms_";
+            };
+            messaging-service = "sql";
+          };
         };
         symlinks = {
           "plugins/Geyser.jar" = pkgs.fetchurl rec {
@@ -99,15 +121,39 @@ in
           "plugins/LimboAPI.jar" = pkgs.fetchurl rec {
             pname = "LimboAPI";
             version = "1.0.8";
-            url = "https://github.com/Elytrium/${pname}/releases/download/1.0.8/${pname}-plugin-${version}-jdk17.jar";
+            url = "https://github.com/Elytrium/${pname}/releases/download/${version}/${pname}-plugin-${version}-jdk17.jar";
             sha256 = "sha256-qGBBHSEGdUXLDQkCBKn5N28/9Zlazu8/fYrAIvlb0EA=";
           };
           "plugins/LimboAuth.jar" = pkgs.fetchurl rec {
             pname = "LimboAuth";
             version = "1.0.8";
-            url = "https://github.com/Elytrium/${pname}/releases/download/1.0.8/${pname}-${version}-jdk17.jar";
+            url = "https://github.com/Elytrium/${pname}/releases/download/${version}/${pname}-${version}-jdk17.jar";
             sha256 = "sha256-S1u7QHF0n6EhGq++VF7BlbaJ4Y8xpQWR2BuGQBeW+r8=";
           };
+          "plugins/HuskChat.jar" = pkgs.fetchurl rec {
+            pname = "HuskChat";
+            version = "2.3";
+            url = "https://github.com/WiIIiam278/${pname}/releases/download/${version}/${pname}-${version}.jar";
+            sha256 = "sha256-gNgL1rYxpDGJmmBURwYe8t3rTzOOiII3kmmZHd8mFHY=";
+          };
+          "plugins/LuckPerms.jar" = pkgs.fetchurl rec {
+            pname = "LuckPerms";
+            version = "5.4.58";
+            url = "https://download.luckperms.net/1467/velocity/${pname}-Velocity-${version}.jar";
+            sha256 = "sha256-6sOnzQWjxRHbhewiMaPfGzq+eZabcX5/btj0XF/1oMY=";
+          };
+          # Declaratively configure initial permission schema
+          # Import with /lpv import initial.json.gz
+          "plugins/luckperms/initial.json.gz" = lib'.gzipFile (lib'.toJSONFile {
+            groups = {
+              admin.nodes = [{
+                type = "prefix";
+                key = "prefix.0.ADM";
+                value = true;
+              }];
+              default.nodes = [ ];
+            };
+          });
         };
       };
 
@@ -120,6 +166,11 @@ in
           online-mode = false;
         };
         files = {
+          "ops.json" = lib'.toJSONFile [{
+            uuid = "3fc76c64-b1b2-4a95-b3cf-0d7d94db2d75";
+            name = "Misterio7x";
+            level = 4;
+          }];
           "config/paper-global.yml" = lib'.toYAMLFile {
             proxies.velocity = {
               enabled = true;
@@ -140,6 +191,18 @@ in
           "plugins/ViaVersion/config.yml" = lib'.toYAMLFile {
             checkforupdates = false;
           };
+          "plugins/LuckPerms/config.yml" = lib'.toYAMLFile {
+            server = "survival";
+            storage-method = "mysql";
+            data = {
+              address = "127.0.0.1";
+              database = "minecraft";
+              username = "minecraft";
+              password = "@DATABASE_PASSWORD@";
+              table-prefix = "luckperms_";
+            };
+            messaging-service = "sql";
+          };
         };
         symlinks = {
           "plugins/ViaVersion.jar" = pkgs.fetchurl rec {
@@ -153,6 +216,12 @@ in
             version = "4.5.1";
             url = "https://github.com/ViaVersion/${pname}/releases/download/${version}/${pname}-${version}.jar";
             sha256 = "sha256-wugRc0J2+oche6pI0n97+SabTOmGGDvamBItbl1neuU=";
+          };
+          "plugins/LuckPerms.jar" = pkgs.fetchurl rec {
+            pname = "LuckPerms";
+            version = "5.4.58";
+            url = "https://download.luckperms.net/1467/bukkit/loader/${pname}-Bukkit-${version}.jar";
+            sha256 = "sha256-roi16xTu+04ofFccuSLwFl/UqfvG0flHDq0R9/20oIM=";
           };
         };
       };
--- hosts/electra/services/minecraft/lib.nix
+++ hosts/electra/services/minecraft/lib.nix
@@ -1,14 +1,14 @@
-{ pkgs }:
-let
-  remarshal = "${pkgs.remarshal}/bin/remarshal";
-in
+{ pkgs, lib }:
 rec {
   toJSONFile = expr: builtins.toFile "expr" (builtins.toJSON expr);
   toYAMLFile = expr: pkgs.runCommand "expr.yaml" { } ''
-    ${remarshal} -i ${toJSONFile expr} -o $out -if json -of yaml
+    ${lib.getExe pkgs.remarshal} -i ${toJSONFile expr} -o $out -if json -of yaml
   '';
   toTOMLFile = expr: pkgs.runCommand "expr.toml" { } ''
-    ${remarshal} -i ${toJSONFile expr} -o $out -if json -of toml
+    ${lib.getExe pkgs.remarshal} -i ${toJSONFile expr} -o $out -if json -of toml
+  '';
+  gzipFile = file: pkgs.runCommand "file.gz" { } ''
+    ${lib.getExe pkgs.gzip} ${file} -c > $out
   '';
   aikarFlags = memory: "-Xms${memory} -Xmx${memory} -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1";
   proxyFlags = memory: "-Xms${memory} -Xmx${memory} -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15";
