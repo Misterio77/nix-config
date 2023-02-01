8ae9b938cbd4e9a1e409c725117bdc22e109469b
Gabriel Fontes <hi@m7.rs>
2023-02-01 18:24:37 -0300

    hydra: fix another typo

--- hydra.nix
+++ hydra.nix
@@ -4,7 +4,7 @@ let
 
   notBroken = pkg: !(pkg.meta.broken or false);
   hasPlatform = sys: pkg: elem sys pkg.meta.platforms;
-  filterValidPkgs = sys: pkgs: filterAttrs (_: pkg: hasPlatform sys pkg && notBroken pkg);
+  filterValidPkgs = sys: pkgs: filterAttrs (_: pkg: hasPlatform sys pkg && notBroken pkg) pkgs;
   getCfg = _: cfg: cfg.config.system.build.toplevel;
 in {
   packages = mapAttrs filterValidPkgs outputs.packages;
