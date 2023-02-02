7d2023c7b3246d8fe829fc2bcf9fb37e9172fa3a
Gabriel Fontes <hi@m7.rs>
2023-02-02 00:44:25 -0300

    hosts/nix: downgrade to nix 2.12

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -18,7 +18,9 @@
       warn-dirty = false;
       system-features = [ "kvm" "big-parallel" ];
     };
-    package = pkgs.nixUnstable;
+    # TODO: temporary fix for NixOS/nix#7704
+    package = pkgs.nixVersions.nix_2_12;
+    # package = pkgs.nixUnstable;
     gc = {
       automatic = true;
       dates = "weekly";
