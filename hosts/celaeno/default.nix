d6f5ebcdbbf9b0e6a44d570b69efbfa12e7a1478
Gabriel Fontes <hi@m7.rs>
2023-02-06 18:20:17 -0300

    hosts/celaeno: don't auto optimise store (it's expensive)

--- hosts/celaeno/default.nix
+++ hosts/celaeno/default.nix
@@ -12,6 +12,8 @@
     useDHCP = true;
   };
   system.stateVersion = "22.05";
+  # Slows down write operations considerably
+  nix.settings.auto-optimise-store = false;
 
   boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
 }
