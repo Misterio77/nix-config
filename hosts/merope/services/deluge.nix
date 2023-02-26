a14057b57b79420e95f9bee3f619ee02516e0680
Gabriel Fontes <hi@m7.rs>
2023-02-25 22:20:34 -0300

    wip

--- hosts/merope/services/deluge.nix
+++ hosts/merope/services/deluge.nix
@@ -1,27 +1,36 @@
-{ lib, config, ... }: {
-  services = {
-    deluge = {
-      enable = true;
-      web = {
-        enable = true;
-      };
+{ config, ... }: {
+  services.deluge = {
+    enable = true;
+    declarative = true;
+    authFile = config.sops.secrets.deluge-accounts.path;
+    config = {
+      copy_torrent_file = true;
+      move_completed = true;
+      torrentfiles_location = "/srv/torrents/files";
+      download_location = "/srv/torrents/downloading";
+      move_completed_path = "/srv/torrents/completed";
+      dont_count_slow_torrents = true;
+      # Daemon on 58846
+      allow_remote = true;
+      daemon_port = 58846;
+      # Listen on 6880 only
+      random_port = false;
+      listen_ports = [ 6880 6880 ];
+      # Outgoing is random
+      random_outgoing_ports = true;
     };
-    nginx.virtualHosts = {
-      "deluge.m7.rs" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/".proxyPass = "http://localhost:${toString config.services.deluge.web.port}";
-      };
-    };
+    openFirewall = true; # Forward listen ports
+  };
+
+  sops.secrets.deluge-accounts = {
+    sopsFile = ../secrets.yaml;
+    owner = config.users.users.deluge.name;
+    group = config.users.users.deluge.group;
   };
 
   networking.firewall = {
-    # Remote control port (not port forwarded)
+    # Remote control port
     allowedTCPPorts = [ 58846 ];
-    allowedUDPPorts = [ 58846 ];
-    # Torrent connection ports (port forwarded)
-    allowedTCPPortRanges = [{ from = 6880; to = 6890; }];
-    allowedUDPPortRanges = [{ from = 6880; to = 6890; }];
   };
 
   environment.persistence = {
