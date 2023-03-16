6c4564d94d9884cbc9bfcdee29c2ce4e8c275ad5
Gabriel Fontes <hi@m7.rs>
2023-03-16 17:44:20 -0300

    hosts: alias alcyone to m7.rs

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -1,10 +1,11 @@
 { outputs, lib, config, ... }:
 
 let
+  inherit (config.networking) hostName;
   hosts = outputs.nixosConfigurations;
-  hostname = config.networking.hostName;
   prefix = "/persist";
   pubKey = host: ../../${host}/ssh_host_ed25519_key.pub;
+  gitHost = hosts."alcyone".config.networking.hostName;
 in
 {
   services.openssh = {
@@ -15,9 +16,9 @@ in
       PermitRootLogin = "no";
       # Automatically remove stale sockets
       StreamLocalBindUnlink = "yes";
+      # Allow forwarding ports to everywhere
+      GatewayPorts = "clientspecified";
     };
-    # Allow forwarding ports to everywhere
-    gatewayPorts = "clientspecified";
 
     hostKeys = [{
       path = "${prefix}/etc/ssh/ssh_host_ed25519_key";
@@ -30,7 +31,9 @@ in
     knownHosts = builtins.mapAttrs
       (name: _: {
         publicKeyFile = pubKey name;
-        extraHostNames = lib.optional (name == hostname) "localhost";
+        extraHostNames =
+          (lib.optional (name == hostName) "localhost") ++ # Alias for localhost if it's the same host
+          (lib.optionals (name == gitHost) [ "m7.rs" "git.m7.rs" ]); # Alias for m7.rs and git.m7.rs if it's the git host
       })
       hosts;
   };
