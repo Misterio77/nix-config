a772a55aebe82e4cbe03a814283cdc34d6140eb6
Gabriel Fontes <hi@m7.rs>
2023-03-31 21:29:04 -0300

    hosts/alcyone: add nix static script

--- hosts/alcyone/services/website/default.nix
+++ hosts/alcyone/services/website/default.nix
@@ -28,6 +28,24 @@ in
           '';
         };
 
+        "=/nix" = {
+          # Script to download static nix
+          alias = pkgs.writeText "nix" ''
+            #/bin/sh
+
+            arch="$(uname -m)"
+            job="https://hydra.nixos.org/job/nix/master/buildStatic.$arch-linux/latest/download-by-type/file/binary-dist"
+            echo "Downloading from: $job"
+
+            mkdir -p "$HOME/.local/bin"
+            curl -L "$job" -o "$HOME/.local/bin/nix"
+            chmod +x "$HOME/.local/bin/nix"
+
+            mkdir -p "$HOME/.config/nix"
+            echo "experimental-features = nix-command flakes" >> "$HOME/.config/nix/nix.conf"
+          '';
+        };
+
         "=/7088C7421873E0DB97FF17C2245CAB70B4C225E9.asc" = {
           alias = pgpKey;
         };
