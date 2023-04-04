062ce428c1860728c740f51ecc62a2ce5147a7a0
Gabriel Fontes <hi@m7.rs>
2023-04-04 15:15:35 -0300

    hosts/persistence: don't chown recursively

--- hosts/common/global/optin-persistence.nix
+++ hosts/common/global/optin-persistence.nix
@@ -22,7 +22,7 @@
     mkHomePersist = user: lib.optionalString user.createHome ''
       mkdir -p /persist
       mkdir -p /persist/${user.home}
-      chown -R ${user.name}:${user.group} /persist/${user.home}
+      chown ${user.name}:${user.group} /persist/${user.home}
       chmod ${user.homeMode} /persist/${user.home}
     '';
     createHomes = lib.concatLines (map mkHomePersist (lib.attrValues config.users.users));
