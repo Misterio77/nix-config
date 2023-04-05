cdade12dccc2e13614b6cf90c1b03de620b1468b
Gabriel Fontes <hi@m7.rs>
2023-04-04 23:56:57 -0300

    hosts: fix quiet boot

--- hosts/common/optional/quietboot.nix
+++ hosts/common/optional/quietboot.nix
@@ -18,13 +18,10 @@
     loader.timeout = 0;
     kernelParams = [
       "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "i915.fastboot=1"
       "loglevel=3"
-      "rd.systemd.show_status=false"
+      "systemd.show_status=auto"
+      "udev.log_level=3"
       "rd.udev.log_level=3"
-      "udev.log_priority=3"
       "vt.global_cursor_default=0"
     ];
     consoleLogLevel = 0;
