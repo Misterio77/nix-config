b3d37209c075e38b2fce29db7e6614b51e35502a
Gabriel Fontes <hi@m7.rs>
2023-04-12 14:11:22 -0300

    hosts/alcyone: try and fix headscale derp issue

--- hosts/alcyone/services/headscale.nix
+++ hosts/alcyone/services/headscale.nix
@@ -1,4 +1,7 @@
 { config, lib, ... }:
+let
+  derpPort = 3478;
+in
 {
   services = {
     headscale = {
@@ -27,6 +30,11 @@
           "100.64.0.0/10"
           "fdef:6567:bd7a::/48"
         ];
+        derp.server = {
+          enable = true;
+          region_id = 999;
+          stun_listen_addr = "0.0.0.0:${toString derpPort}";
+        };
       };
     };
 
@@ -51,6 +59,10 @@
       };
     };
   };
+
+  # Derp server
+  networking.firewall.allowedUDPPorts = [ derpPort ];
+
 
   environment.systemPackages = [ config.services.headscale.package ];
 
