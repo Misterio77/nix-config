cdbfe6d91a6cad08a4c468aa676cce6fc226c6fe
Gabriel Fontes <hi@m7.rs>
2023-04-20 20:30:41 -0300

    enable pass-secret-service

--- home/misterio/features/pass/default.nix
+++ home/misterio/features/pass/default.nix
@@ -4,6 +4,12 @@
     settings = { PASSWORD_STORE_DIR = "$HOME/.password-store"; };
     package = pkgs.pass.withExtensions (p: [ p.pass-otp ]);
   };
+
+  services.pass-secret-service = {
+    enable = true;
+    storePath = null;
+    # Use default ($HOME/.password-store)
+    # Passing $HOME is buggy because systemd moment
   };
 
   home.persistence = {
