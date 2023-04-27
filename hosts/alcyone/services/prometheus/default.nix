6148399e6e0b272dda84f2af03fb6e0c1c584b87
Gabriel Fontes <hi@m7.rs>
2023-04-27 20:03:13 -0300

    prometheus: scrape sitespeed

--- hosts/alcyone/services/prometheus/default.nix
+++ hosts/alcyone/services/prometheus/default.nix
@@ -2,6 +2,13 @@
   services = {
     prometheus = {
       enable = true;
+      scrapeConfigs = [{
+        job_name = "sitespeed";
+        scheme = "https";
+        static_configs = [{
+          targets = [ "sitespeed.m7.rs/metrics" ];
+        }];
+      }];
     };
     nginx.virtualHosts = {
       "metrics.m7.rs" = {
