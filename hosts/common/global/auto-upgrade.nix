fb80799cbcb4967f223e567f791f745848c17f57
Gabriel Fontes <hi@m7.rs>
2023-05-15 13:51:46 -0300

    hosts/auto-upgrade: conditionally upgrade
    Only do it if tree is clean, that means that (probably) was built from a deployment, and am not locally working on my config.

--- hosts/common/global/auto-upgrade.nix
+++ hosts/common/global/auto-upgrade.nix
@@ -1,10 +1,13 @@
-{ config, ... }:
+{ config, inputs, ... }:
 let
   inherit (config.networking) hostName;
+  # Only enable auto upgrade if current config came from a clean tree
+  # This avoids accidental auto-upgrades when working locally.
+  isClean = inputs.self ? rev;
 in
 {
   system.autoUpgrade = {
-    enable = true;
+    enable = isClean;
     dates = "hourly";
     flags = [
       "--refresh"
