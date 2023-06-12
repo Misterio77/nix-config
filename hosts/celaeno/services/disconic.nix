978492a90c340b64fdf227f4f7a7b98f17b78f14
Gabriel Fontes <hi@m7.rs>
2023-06-12 09:41:43 -0300

    bump disconic

--- hosts/celaeno/services/disconic.nix
+++ hosts/celaeno/services/disconic.nix
@@ -7,22 +7,11 @@
     enable = true;
     package = pkgs.inputs.disconic.default;
     user = "disconic";
-
-    subsonicUrl = "https://music.m7.rs";
-    subsonicUser = "misterio";
-    discordGuild = "324685449937420288";
-
-    subsonicPasswordFile = config.sops.secrets.disconic-ss-password.path;
-    discordTokenFile = config.sops.secrets.disconic-discord-token.path;
+    environmentFile = config.sops.secrets.disconic-secrets.path;
   };
 
   sops.secrets = {
-    disconic-ss-password = {
-      owner = "disconic";
-      group = "disconic";
-      sopsFile = ../secrets.yaml;
-    };
-    disconic-discord-token = {
+    disconic-secrets = {
       owner = "disconic";
       group = "disconic";
       sopsFile = ../secrets.yaml;
