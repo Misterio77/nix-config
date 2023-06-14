b801500bcf3eb803837ae00c3897993afe63f0f2
Gabriel Fontes <hi@m7.rs>
2023-06-14 07:57:49 -0300

    fix prismlauncher theming

--- home/misterio/features/desktop/common/qt.nix
+++ home/misterio/features/desktop/common/qt.nix
@@ -1,6 +1,11 @@
+{ pkgs, ... }:
 {
   qt = {
     enable = true;
     platformTheme = "gtk";
+    style = {
+      name = "gtk2";
+      package = pkgs.libsForQt5.qtstyleplugins;
+    };
   };
 }
--- home/misterio/features/games/prism-launcher.nix
+++ home/misterio/features/games/prism-launcher.nix
@@ -1,5 +1,5 @@
 { pkgs, lib, ... }: {
-  home.packages = [ pkgs.prismlauncher ];
+  home.packages = [ pkgs.prismlauncher-qt5 ];
 
   home.persistence = {
     "/persist/home/misterio".directories = [ ".local/share/PrismLauncher" ];
