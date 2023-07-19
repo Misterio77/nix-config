ffc141db8000197a9eede52a55df6391ceb2919e
Gabriel Fontes <hi@m7.rs>
2023-07-19 10:40:12 -0300

    remove secondary monitors from electra

--- home/misterio/electra.nix
+++ home/misterio/electra.nix
@@ -18,20 +18,6 @@
       workspace = "3";
       x = 0;
     }
-    {
-      name = "DP-2";
-      width = 1920;
-      height = 1080;
-      workspace = "1";
-      x = 1920;
-    }
-    {
-      name = "HDMI-A-1";
-      width = 1920;
-      height = 1080;
-      workspace = "2";
-      x = 3840;
-    }
   ];
 
   # programs.git.userEmail = "gabriel@zoocha.com";
