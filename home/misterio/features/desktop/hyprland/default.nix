9499e522ad28f229312302bb0f734e125f60e819
Gabriel Fontes <hi@m7.rs>
2023-07-30 21:20:45 -0300

    hyprland: add bouncy beziers

--- home/misterio/features/desktop/hyprland/default.nix
+++ home/misterio/features/desktop/hyprland/default.nix
@@ -55,20 +55,21 @@
         bezier = [
           "easein,0.11, 0, 0.5, 0"
           "easeout,0.5, 1, 0.89, 1"
-          "easeinout,0.45, 0, 0.55, 1"
+          "easeinback,0.36, 0, 0.66, -0.56"
+          "easeoutback,0.34, 1.56, 0.64, 1"
         ];
 
         animation = [
-          "windowsIn,1,3,easeout,slide"
-          "windowsOut,1,3,easein,slide"
-          "windowsMove,1,3,easeout"
+          "windowsIn,1,3,easeoutback,slide"
+          "windowsOut,1,3,easeinback,slide"
+          "windowsMove,1,3,easeoutback"
+          "workspaces,1,2,easeoutback,slide"
           "fadeIn,1,3,easeout"
           "fadeOut,1,3,easein"
           "fadeSwitch,1,3,easeout"
           "fadeShadow,1,3,easeout"
           "fadeDim,1,3,easeout"
           "border,1,3,easeout"
-          "workspaces,1,2,easeout,slide"
         ];
       };
 
