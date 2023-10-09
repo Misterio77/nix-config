22220c5e1278d84472908389829ff56068426220
Gabriel Fontes <hi@m7.rs>
2023-10-09 14:22:57 -0300

    home/nvim: use nvim-cmp default keybinds
    That do exactly what I wanted

--- home/misterio/features/nvim/lsp.nix
+++ home/misterio/features/nvim/lsp.nix
@@ -77,14 +77,13 @@
         local cmp = require('cmp')
 
         cmp.setup{
-          formatting = { format = require('lspkind').cmp_format() },
-          -- Same keybinds as vim's vanilla completion
-          mapping = {
-            ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-            ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-            ['<C-e>'] = cmp.mapping.close(),
-            ['<C-y>'] = cmp.mapping.confirm(),
+          formatting = {
+            format = require('lspkind').cmp_format({
+            })
           },
+          -- Same keybinds as vim's vanilla completion
+          mapping = cmp.mapping.preset.insert({
+          }),
           sources = {
             { name='buffer', option = { get_bufnrs = vim.api.nvim_list_bufs } },
             { name='nvim_lsp' },
