46deb6cfde1d135f378e23a281ef494282713002
Gabriel Fontes <hi@m7.rs>
2023-10-22 17:36:08 -0300

    home/nvim/indent-blankline: update config

--- home/misterio/features/nvim/ui.nix
+++ home/misterio/features/nvim/ui.nix
@@ -81,7 +81,10 @@
       plugin = indent-blankline-nvim;
       type = "lua";
       config = /* lua */ ''
-        require('indent_blankline').setup{char_highlight_list={'IndentBlankLine'}}
+        require('ibl').setup{
+          scope = { highlight = {"IndentBlankLine"} },
+          indent = { highlight = {"IndentBlankLine"} },
+        }
       '';
     }
     {
