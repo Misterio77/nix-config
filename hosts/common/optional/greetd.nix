dc45c531ea4550732c17070a6659048d8c8e8cb7
Gabriel Fontes <hi@m7.rs>
2023-11-26 18:21:57 -0300

    greetd: replace sway with cage

--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -1,43 +1,51 @@
 { pkgs, lib, config, ... }:
 let
   homeCfgs = config.home-manager.users;
-  homePaths = lib.mapAttrsToList (n: v: "${v.home.path}/share") homeCfgs;
-  extraDataPaths = lib.concatStringsSep ":" homePaths;
-  vars = ''XDG_DATA_DIRS="$XDG_DATA_DIRS:${extraDataPaths}"'';
+  homeSharePaths = lib.mapAttrsToList (n: v: "${v.home.path}/share") homeCfgs;
+  vars = ''XDG_DATA_DIRS="$XDG_DATA_DIRS:${lib.concatStringsSep ":" homeSharePaths}"'';
 
-  sway-kiosk = command: "${pkgs.sway}/bin/sway --config ${pkgs.writeText "kiosk.config" ''
-    output * bg #000000 solid_color
-    exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK"
-    xwayland disable
-    input "type:touchpad" {
-      tap enabled
-    }
-    exec '${vars} ${command} -l debug; ${pkgs.sway}/bin/swaymsg exit'
-  ''}";
-
+  # TODO: this should not be coupled to my home config
+  # Or at least have some kind of fallback values if it's not present on this machine
   misterioCfg = homeCfgs.misterio;
+  mainMonitor = lib.head (lib.filter (x: x.primary) misterioCfg.monitors);
+  gtkTheme = misterioCfg.gtk.theme;
+  iconTheme = misterioCfg.gtk.iconTheme.package;
+  wallpaper = misterioCfg.wallpaper;
+
+  wlr-randr = lib.getExe pkgs.wlr-randr;
+  grep = lib.getExe pkgs.gnugrep;
+  cage-kiosk = command: "${lib.getExe pkgs.cage} -s -- ${pkgs.writeShellScript "cage-cmd" ''
+    # Turn off every monitor, except the main one
+    ${wlr-randr} | ${grep} '^\S' | cut -d ' ' -f1 | \
+    ${grep} -v ${mainMonitor.name} | \
+    while IFS= read -r output; do
+      echo "Turning off $output" >> /tmp/greetd-cage.log
+      ${wlr-randr} --output $output --off
+    done
+    exec ${vars} ${command}
+  ''}";
 in
 {
   users.extraUsers.greeter.packages = [
-    misterioCfg.gtk.theme.package
-    misterioCfg.gtk.iconTheme.package
+    gtkTheme.package
+    iconTheme.package
   ];
 
   programs.regreet = {
     enable = true;
     settings = {
       GTK = {
-        icon_theme_name = "ePapirus";
-        theme_name = misterioCfg.gtk.theme.name;
+        icon_theme_name = "Papirus";
+        theme_name = gtkTheme.name;
       };
       background = {
-        path = misterioCfg.wallpaper;
+        path = wallpaper;
         fit = "Cover";
       };
     };
   };
   services.greetd = {
     enable = true;
-    settings.default_session.command = sway-kiosk (lib.getExe config.programs.regreet.package);
+    settings.default_session.command = cage-kiosk (lib.getExe config.programs.regreet.package);
   };
 }
