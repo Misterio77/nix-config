4a323a8579d6e9811df5e3afbd90a070335d0eb6
Gabriel Fontes <hi@m7.rs>
2023-12-18 15:35:08 -0300

    electra monitors

--- home/misterio/electra.nix
+++ home/misterio/electra.nix
@@ -12,11 +12,18 @@
 
   monitors = [
     {
+      name = "HDMI-A-1";
+      width = 1920;
+      height = 1080;
+      workspace = "2";
+      x = 0;
+    }
+    {
       name = "eDP-1";
       width = 1920;
       height = 1080;
-      workspace = "3";
-      x = 0;
+      workspace = "1";
+      x = 1920;
       primary = true;
     }
   ];
