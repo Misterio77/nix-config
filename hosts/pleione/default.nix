1b783db5464a2efddfdae2afc062adb61e701d13
Gabriel Fontes <hi@m7.rs>
2023-12-24 13:59:27 -0300

    pleione: enable aarch and i686

--- hosts/pleione/default.nix
+++ hosts/pleione/default.nix
@@ -22,6 +22,7 @@
 
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+    binfmt.emulatedSystems = [ "aarch64-linux" "i686-linux" ];
   };
 
   powerManagement.powertop.enable = true;
