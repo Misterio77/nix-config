6c9b2d1ce9449544cdee288ec2f3b59452a2f254
Gabriel Fontes <hi@m7.rs>
2023-12-24 12:18:05 -0300

    celaeno/minecraft: update geyser+floodgate

--- hosts/celaeno/services/minecraft/servers/proxy/geyser.nix
+++ hosts/celaeno/services/minecraft/servers/proxy/geyser.nix
@@ -1,17 +1,19 @@
-{ pkgs, lib, ... }: {
+{ pkgs, ... }: let
+  geyserUrl = n: v: b: "https://download.geysermc.org/v2/projects/${n}/versions/${v}/builds/${b}/downloads/velocity";
+in {
   services.minecraft-servers.servers.proxy = {
     symlinks = {
       "plugins/Geyser.jar" = pkgs.fetchurl rec {
-        pname = "Geyser";
-        version = "1498";
-        url = "https://ci.opencollab.dev/job/GeyserMC/job/${pname}/job/master/${version}/artifact/bootstrap/velocity/build/libs/${pname}-Velocity.jar";
-        hash = "sha256-5mvh8rB+1xwdDuXj98TR2RLs173RzLR1tHsksfMwAjI=";
+        pname = "geyser";
+        version = "2.2.0";
+        url = geyserUrl pname version "408";
+        hash = "sha256-aFxd2OzE0daWSEiWP/+4LeLoJz0VXSG2E+zmM9XHPT4=";
       };
-      "plugins/Floodgate.jar" = pkgs.fetchurl rec {
-        pname = "Floodgate";
-        version = "92";
-        url = "https://ci.opencollab.dev/job/GeyserMC/job/${pname}/job/master/${version}/artifact/velocity/build/libs/${lib.toLower pname}-velocity.jar";
-        hash = "sha256-U2TLrXnSL6RkKDAMV5dTQ80yDF9DVdPJORGoKJzAP0M=";
+      "plugins/Floodgate.jar" =  pkgs.fetchurl rec {
+        pname = "floodgate";
+        version = "2.2.2";
+        url = geyserUrl pname version "85";
+        hash = "sha256-Mw0bsoj2zCwqpuuY/cpM1Krcue0XPeybmF6/UA/udWk=";
       };
     };
     files = {
