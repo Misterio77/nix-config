91fc64b8a10fc1e5f07237b950f1cd8a7f802af8
Gabriel Fontes <hi@m7.rs>
2023-12-25 21:24:34 -0300

    celaeno: remove sitespeed (and its metrics in prometheus)

--- hosts/alcyone/services/prometheus.nix
+++ hosts/alcyone/services/prometheus.nix
@@ -29,34 +29,6 @@
             targets = [ "alcyone.m7.rs" "celaeno.m7.rs" "merope.m7.rs" ];
           }];
         }
-        {
-          job_name = "sitespeed";
-          scheme = "https";
-          static_configs = [{
-            targets = [ "sitespeed.m7.rs" ];
-          }];
-          metric_relabel_configs = [
-            # Only keep metrics that are not aggregations or are medians
-            {
-              source_labels = [ "aggr_kind" ];
-              regex = "(median|)";
-              action = "keep";
-            }
-            # Then remove the aggregation label
-            {
-              regex = "aggr_kind";
-              action = "labeldrop";
-            }
-
-            # Drop {content,header,transfer}Size that don't have a content_type and/or content_origin
-            # They're just totals, we can obtain them by summing the individual parts
-            {
-              source_labels = [ "__name__" "content_origin" "content_type" ];
-              regex = "sitespeedio_pagexray_((content|header|transfer)Size|cookies|requests)(;;|;[^;]*;|;;[^;]*)";
-              action = "drop";
-            }
-          ];
-        }
       ];
       extraFlags = let prometheus = config.services.prometheus.package;
       in [
