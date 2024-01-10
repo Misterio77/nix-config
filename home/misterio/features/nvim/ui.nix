eaf6de56f0e79eca4a1eca2257a274807715472e
Gabriel Fontes <hi@m7.rs>
2024-01-10 09:44:53 -0300

    home/nvim: fix missing scratchpad CR

--- home/misterio/features/nvim/ui.nix
+++ home/misterio/features/nvim/ui.nix
@@ -41,8 +41,8 @@
             dashboard.button( "n", "󰈔 New file" , ":enew<CR>"),
             dashboard.button( "e", " Explore", ":Explore<CR>"),
             dashboard.button( "g", " Git summary", ":Git | :only<CR>"),
-            dashboard.button( "s", " Scratchpad", ":e ~/Documents/Notes/scratch.md"),
-            dashboard.button( "c", "  Nix config flake" , ":cd ~/Documents/NixConfig | :e flake.nix<CR>"),
+            dashboard.button( "s", " Scratchpad", ":e ~/Documents/Notes/scratch.md<CR>"),
+            dashboard.button( "c", "  Nix config flake" , ":e ~/Documents/NixConfig/flake.nix<CR>"),
             dashboard.button( "q", "󰅙  Quit nvim", ":qa<CR>"),
         }
 
