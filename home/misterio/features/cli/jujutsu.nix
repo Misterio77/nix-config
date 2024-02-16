8dda904bbd720bdd12a97c0b58ccc23333b8f004
Gabriel Fontes <hi@m7.rs>
2024-02-16 13:37:07 -0300

    home/jujutsu: disable shell integration

--- home/misterio/features/cli/jujutsu.nix
+++ home/misterio/features/cli/jujutsu.nix
@@ -2,6 +2,9 @@
 {
   programs.jujutsu = {
     enable = true;
+    enableBashIntegration = false;
+    enableZshIntegration = false;
+    enableFishIntegration = false;
     settings = {
       user = {
         name = config.programs.git.userName;
