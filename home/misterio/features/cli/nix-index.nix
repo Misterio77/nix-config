f040b523b2afaf09a43a9ce6bb6176f4db45e4b1
Gabriel Fontes <hi@m7.rs>
2024-02-16 13:36:57 -0300

    home/nix-index: simplify script

--- home/misterio/features/cli/nix-index.nix
+++ home/misterio/features/cli/nix-index.nix
@@ -1,35 +1,32 @@
-{ pkgs, ... }:
-let
-  update-script = pkgs.writeShellApplication {
-    name = "fetch-nix-index-database";
-    runtimeInputs = with pkgs; [ wget coreutils ];
-    text = ''
-      filename="index-x86_64-linux"
-      mkdir -p ~/.cache/nix-index
-      cd ~/.cache/nix-index
-      wget -N "https://github.com/Mic92/nix-index-database/releases/latest/download/$filename"
-      ln -f "$filename" files
-    '';
-  };
-in
+{ pkgs, lib, ... }:
 {
   programs.nix-index.enable = true;
 
   systemd.user.services.nix-index-database-sync = {
-    Unit = { Description = "fetch mic92/nix-index-database"; };
+    Unit.Description = "fetch mic92/nix-index-database";
     Service = {
       Type = "oneshot";
-      ExecStart = "${update-script}/bin/fetch-nix-index-database";
+      ExecStart = lib.getExe (pkgs.writeShellApplication {
+        name = "fetch-nix-index-database";
+        runtimeInputs = with pkgs; [ wget coreutils ];
+        text = ''
+          mkdir -p ~/.cache/nix-index
+          cd ~/.cache/nix-index
+          name="index-${pkgs.stdenv.system}"
+          wget -N "https://github.com/Mic92/nix-index-database/releases/latest/download/$name"
+          ln -sf "$name" "files"
+        '';
+      });
       Restart = "on-failure";
       RestartSec = "5m";
     };
   };
   systemd.user.timers.nix-index-database-sync = {
-    Unit = { Description = "Automatic github:mic92/nix-index-database fetching"; };
+    Unit.Description = "Automatic github:mic92/nix-index-database fetching";
     Timer = {
       OnBootSec = "10m";
       OnUnitActiveSec = "24h";
     };
-    Install = { WantedBy = [ "timers.target" ]; };
+    Install.WantedBy = [ "timers.target" ];
   };
 }
