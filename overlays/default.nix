9dc13420b9832f6120387fd7a0850bee47c72541
Gabriel Fontes <hi@m7.rs>
2024-02-16 17:37:22 -0300

    Revert "hydra: drop hydra-restrict-eval patch"
    This reverts commit a17e653fa82504117c5ca76bef3a1e23f2d815ef.

--- overlays/default.nix
+++ overlays/default.nix
@@ -42,6 +42,9 @@ in
     # https://github.com/mdellweg/pass_secret_service/pull/37
     pass-secret-service = addPatches prev.pass-secret-service [ ./pass-secret-service-native.diff ];
 
+    # https://github.com/NixOS/nix/issues/7098
+    hydra_unstable = addPatches prev.hydra_unstable [ ./hydra-restrict-eval.diff ];
+
     xdg-utils-spawn-terminal = final.callPackage ../pkgs/xdg-utils { };
 
     pfetch = prev.pfetch.overrideAttrs (oldAttrs: {
