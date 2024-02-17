cf94d7bf024e38d34fa9b5bedae814f277b086dd
Gabriel Fontes <hi@m7.rs>
2024-02-17 12:40:19 -0300

    home/git: add some nice git options
    Better conflict display, show diff when commiting, better diff algo, ISO
    dates, autosetup remote, reuse merge fixes

--- home/misterio/features/cli/git.nix
+++ home/misterio/features/cli/git.nix
@@ -48,6 +48,15 @@ in
       user.signing.key = "CE707A2C17FAAC97907FF8EF2E54EA7BFE630916";
       commit.gpgSign = true;
       gpg.program = "${config.programs.gpg.package}/bin/gpg2";
+
+      merge.conflictStyle = "zdiff3";
+      commit.verbose = true;
+      diff.algorithm = "histogram";
+      log.date = "iso";
+      # Automatically track remote branch
+      push.autoSetupRemote = true;
+      # Reuse merge conflict fixes when rebasing
+      rerere.enabled = true;
     };
     lfs.enable = true;
     ignores = [ ".direnv" "result" ];
