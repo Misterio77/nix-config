098b05f218edb9936bfe08b1bef8890c42775c74
Gabriel Fontes <hi@m7.rs>
2024-03-18 12:35:29 -0300

    alcyone: fix website themes

--- hosts/alcyone/services/website/themes.nix
+++ hosts/alcyone/services/website/themes.nix
@@ -15,22 +15,22 @@ let
       :root {
         --scheme-name: \"\(.name)\";
         --scheme-author: \"\(.author)\";
-        --base00: #\(.colors.base00);
-        --base01: #\(.colors.base01);
-        --base02: #\(.colors.base02);
-        --base03: #\(.colors.base03);
-        --base04: #\(.colors.base04);
-        --base05: #\(.colors.base05);
-        --base06: #\(.colors.base06);
-        --base07: #\(.colors.base07);
-        --base08: #\(.colors.base08);
-        --base09: #\(.colors.base09);
-        --base0A: #\(.colors.base0A);
-        --base0B: #\(.colors.base0B);
-        --base0C: #\(.colors.base0C);
-        --base0D: #\(.colors.base0D);
-        --base0E: #\(.colors.base0E);
-        --base0F: #\(.colors.base0F);
+        --base00: #\(.palette.base00);
+        --base01: #\(.palette.base01);
+        --base02: #\(.palette.base02);
+        --base03: #\(.palette.base03);
+        --base04: #\(.palette.base04);
+        --base05: #\(.palette.base05);
+        --base06: #\(.palette.base06);
+        --base07: #\(.palette.base07);
+        --base08: #\(.palette.base08);
+        --base09: #\(.palette.base09);
+        --base0A: #\(.palette.base0A);
+        --base0B: #\(.palette.base0B);
+        --base0C: #\(.palette.base0C);
+        --base0D: #\(.palette.base0D);
+        --base0E: #\(.palette.base0E);
+        --base0F: #\(.palette.base0F);
       }
         "' <<< "$scheme" > "$scheme_name.css"
       }
