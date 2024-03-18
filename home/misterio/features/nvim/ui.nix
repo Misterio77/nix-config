048a37859d86e92889f129db593aaa4e4abd6597
Gabriel Fontes <hi@m7.rs>
2024-03-18 11:13:07 -0300

    nvim: add gx.nvim (to fix oil.nvim breaking gx)

--- home/misterio/features/nvim/ui.nix
+++ home/misterio/features/nvim/ui.nix
@@ -1,5 +1,25 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+  gx-nvim = pkgs.vimUtils.buildVimPlugin {
+    name = "gx-nvim";
+    src = pkgs.fetchFromGitHub {
+      owner = "chrishrb";
+      repo = "gx.nvim";
+      rev = "f29a87454b02880e0d76264c21be8316224a7395";
+      hash = "sha256-QWJ/cPvSyMTJoWLg51BNFf9+/9i7G+nzennpHP/eQ4g=";
+    };
+  };
+in {
   programs.neovim.plugins = with pkgs.vimPlugins; [
+    {
+      plugin = gx-nvim;
+      type = "lua";
+      config = /* lua */ ''
+        require('gx').setup{}
+        vim.keymap.set({"n", "x"}, "gx", ":Browse<CR>", {
+          desc = "Open the file under cursor with system app"
+        })
+      '';
+    }
     # UI
     vim-illuminate
     vim-numbertoggle
