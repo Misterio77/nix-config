9fb6e98f5156742fae2d37c8885864be25f85b48
Gabriel Fontes <hi@m7.rs>
2024-03-18 11:13:32 -0300

    nvim: move oil.nvim to ui.nix

--- home/misterio/features/nvim/default.nix
+++ home/misterio/features/nvim/default.nix
@@ -147,33 +147,6 @@ in
           require('nvim-autopairs').setup{}
         '';
       }
-      {
-        plugin = oil-nvim;
-        type = "lua";
-        config = /* lua */ ''
-        require('oil').setup{
-          buf_options = {
-            buflisted = true,
-            bufhidden = "delete",
-          },
-          cleanup_delay_ms = false,
-          use_default_keymaps = false,
-          keymaps = {
-            ["<CR>"] = "actions.select",
-            ["-"] = "actions.parent",
-            ["_"] = "actions.open_cwd",
-            ["`"] = "actions.cd",
-            ["~"] = "actions.tcd",
-            ["gc"] = "actions.close",
-            ["gr"] = "actions.refresh",
-            ["gs"] = "actions.change_sort",
-            ["gx"] = "actions.open_external",
-            ["g."] = "actions.toggle_hidden",
-            ["g\\"] = "actions.toggle_trash",
-          },
-        }
-        '';
-      }
     ];
   };
 
