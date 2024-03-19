e079f6be97574388aaa573c84990dcc2a93174e1
Gabriel Fontes <hi@m7.rs>
2024-03-19 13:41:36 -0300

    go back to usual nix-serve nix version

--- hosts/celaeno/services/binary-cache.nix
+++ hosts/celaeno/services/binary-cache.nix
@@ -8,8 +8,7 @@
     nix-serve = {
       enable = true;
       secretKeyFile = config.sops.secrets.cache-sig-key.path;
-      # TODO: temporary fix for NixOS/nix#7704
-      package = pkgs.nix-serve.override { nix = pkgs.nixVersions.nix_2_12; };
+      package = pkgs.nix-serve;
     };
     nginx.virtualHosts."cache.m7.rs" = {
       forceSSL = true;
