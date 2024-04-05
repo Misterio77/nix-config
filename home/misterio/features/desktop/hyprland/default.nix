bf26e9a1a71f57e7991a882d6ff76d93a1aff631
Gabriel Fontes <hi@m7.rs>
2024-04-05 19:25:24 -0300

    home/hyprland: nice bezier curves for layer anims

--- home/misterio/features/desktop/hyprland/default.nix
+++ home/misterio/features/desktop/hyprland/default.nix
@@ -76,15 +76,10 @@
       layerrule = [
         "blur,waybar"
         "ignorezero,waybar"
-        "animation slide,waybar"
-
         "blur,notifications"
         "ignorezero,notifications"
-        "animation slide,notifications"
-
         "blur,wofi"
         "ignorezero,wofi"
-        "animation slide,wofi"
       ];
 
       decoration = {
@@ -111,21 +106,27 @@
         bezier = [
           "easein,0.11, 0, 0.5, 0"
           "easeout,0.5, 1, 0.89, 1"
+          "easeinout,0.45, 0, 0.55, 1"
           "easeinback,0.36, 0, 0.66, -0.56"
           "easeoutback,0.34, 1.56, 0.64, 1"
+          "easeinoutback,0.68, -0.6, 0.32, 1.6"
         ];
 
         animation = [
+          "border,1,3,easeout"
+          "workspaces,1,2,easeoutback,slide"
           "windowsIn,1,3,easeoutback,slide"
           "windowsOut,1,3,easeinback,slide"
           "windowsMove,1,3,easeoutback"
-          "workspaces,1,2,easeoutback,slide"
           "fadeIn,1,3,easeout"
           "fadeOut,1,3,easein"
-          "fadeSwitch,1,3,easeout"
-          "fadeShadow,1,3,easeout"
-          "fadeDim,1,3,easeout"
-          "border,1,3,easeout"
+          "fadeSwitch,1,3,easeinout"
+          "fadeShadow,1,3,easeinout"
+          "fadeDim,1,3,easeinout"
+          "fadeLayersIn,1,3,easeoutback"
+          "fadeLayersOut,1,3,easeinback"
+          "layersIn,1,3,easeoutback,slide"
+          "layersOut,1,3,easeinback,slide"
         ];
       };
 
