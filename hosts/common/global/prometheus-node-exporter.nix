94d81839f8777f435c22dd45699916581ae8b8a9
Gabriel Fontes <hi@m7.rs>
2024-04-06 21:23:14 -0300

    prometheus: enable systemd exporter

--- hosts/common/global/prometheus-node-exporter.nix
+++ hosts/common/global/prometheus-node-exporter.nix
@@ -2,6 +2,7 @@
 {
   services.prometheus.exporters.node = {
     enable = true;
+    enabledCollectors = [ "systemd" ];
   };
   networking.firewall.interfaces."tailscale0" = {
     allowedTCPPorts = [ config.services.prometheus.exporters.node.port ];
