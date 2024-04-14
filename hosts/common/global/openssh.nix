b27c2ee029eb861a9cd5def5c535c79ed25a7c3a
Gabriel Fontes <hi@m7.rs>
2024-04-13 23:50:15 -0300

    hosts/ssh: replace pam_ssh_agent_auth with pam_rssh

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -2,6 +2,7 @@
   outputs,
   lib,
   config,
+  pkgs,
   ...
 }: let
   inherit (config.networking) hostName;
@@ -50,8 +51,20 @@ in {
   };
 
   # Passwordless sudo when SSH'ing with keys
-  security.pam.sshAgentAuth = {
-    enable = true;
-    authorizedKeysFiles = ["/etc/ssh/authorized_keys.d/%u"];
+  security.pam.services.sudo = {config, ...}: {
+    rules.auth.rssh = {
+      order = config.rules.auth.ssh_agent_auth.order - 1;
+      control = "sufficient";
+      modulePath = "${pkgs.pam_rssh}/lib/libpam_rssh.so";
+      settings.authorized_keys_command =
+        pkgs.writeShellScript "get-authorized-keys"
+        ''
+          cat "/etc/ssh/authorized_keys.d/$1"
+        '';
+    };
   };
+  # Keep SSH_AUTH_SOCK when sudo'ing
+  security.sudo.extraConfig = ''
+    Defaults env_keep+=SSH_AUTH_SOCK
+  '';
 }
