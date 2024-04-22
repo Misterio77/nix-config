eebddcf5fcd310179b115b4586a299dd193ec3e1
Gabriel Fontes <hi@m7.rs>
2024-04-22 20:00:57 -0300

    fix pfetch config

--- home/misterio/features/cli/pfetch.nix
+++ home/misterio/features/cli/pfetch.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   home = {
     packages = with pkgs; [pfetch-rs];
-    sessionVariables.PF_INFO = "ascii title os kernel uptime shell term de palette";
+    sessionVariables.PF_INFO = "ascii title os kernel uptime shell de palette";
   };
 }
