870b3bd3697b1e646e78cb9d34c1e73ad618579e
Gabriel Fontes <hi@m7.rs>
2024-04-22 13:32:21 -0300

    don't include functions in hydra eval

--- hydra.nix
+++ hydra.nix
@@ -7,9 +7,15 @@
   notBroken = pkg: !(pkg.meta.broken or false);
   isDistributable = pkg: (pkg.meta.license or {redistributable = true;}).redistributable;
   hasPlatform = sys: pkg: lib.elem sys (pkg.meta.platforms or [sys]);
-  filterValidPkgs = sys: pkgs: lib.filterAttrs (_: pkg: hasPlatform sys pkg && notBroken pkg && isDistributable pkg) pkgs;
+  filterValidPkgs = sys: pkgs:
+    lib.filterAttrs (_: pkg:
+      lib.isDerivation pkg
+      && hasPlatform sys pkg
+      && notBroken pkg
+      && isDistributable pkg)
+    pkgs;
 
-  getConfigTopLevel = (_: cfg: cfg.config.system.build.toplevel);
+  getConfigTopLevel = _: cfg: cfg.config.system.build.toplevel;
 in {
   pkgs = lib.mapAttrs filterValidPkgs outputs.packages;
   hosts = lib.mapAttrs getConfigTopLevel outputs.nixosConfigurations;
