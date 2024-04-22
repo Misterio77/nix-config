0f6ff8b963cb5d4d57677bda26ebd2b845ac13a5
Gabriel Fontes <hi@m7.rs>
2024-04-22 13:15:52 -0300

    make hydra build when platforms is []

--- hydra.nix
+++ hydra.nix
@@ -6,7 +6,7 @@
 
   notBroken = pkg: !(pkg.meta.broken or false);
   isDistributable = pkg: (pkg.meta.license or {redistributable = true;}).redistributable;
-  hasPlatform = sys: pkg: lib.elem sys (pkg.meta.platforms or []);
+  hasPlatform = sys: pkg: lib.elem sys (pkg.meta.platforms or [sys]);
   filterValidPkgs = sys: pkgs: lib.filterAttrs (_: pkg: hasPlatform sys pkg && notBroken pkg && isDistributable pkg) pkgs;
 
   getConfigTopLevel = (_: cfg: cfg.config.system.build.toplevel);
