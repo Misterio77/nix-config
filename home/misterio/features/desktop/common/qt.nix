99371ce59be149e91ac16d0d810ff0ac65196330
Gabriel Fontes <hi@m7.rs>
2024-05-05 12:21:58 -0300

    feat(home/qt): use native gtk3 platform theme plugin

--- home/misterio/features/desktop/common/qt.nix
+++ home/misterio/features/desktop/common/qt.nix
@@ -4,10 +4,8 @@
 }: {
   qt = {
     enable = true;
-    platformTheme = "gtk";
-    style = {
-      name = "gtk2";
-      package = pkgs.qt6Packages.qt6gtk2;
+    platformTheme = {
+      name = "gtk3";
     };
   };
 }
