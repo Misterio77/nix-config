994c4c49cc62c42a58d1824ddb6fdc2a75d38886
Gabriel Fontes <hi@m7.rs>
2024-05-05 16:08:17 -0300

    fix(flake): use nix-systems/default-linux

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     # Nix ecosystem
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
-    systems.url = "github:nix-systems/default";
+    systems.url = "github:nix-systems/default-linux";
 
     nix = {
       url = "github:nixos/nix/2.22-maintenance";
