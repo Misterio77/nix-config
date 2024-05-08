be5e6496b8d2fea3ef4cf51b167acf44656577e0
Gabriel Fontes <hi@m7.rs>
2024-05-08 12:35:55 -0300

    feat(home/hyprland): add mako restore keybind
    To see old notifications

--- home/misterio/features/desktop/hyprland/default.nix
+++ home/misterio/features/desktop/hyprland/default.nix
@@ -213,7 +213,10 @@
           let
             makoctl = lib.getExe' config.services.mako.package "makoctl";
           in
-            lib.optionals config.services.mako.enable ["SUPER,w,exec,${makoctl} dismiss"]
+            lib.optionals config.services.mako.enable [
+              "SUPER,w,exec,${makoctl} dismiss"
+              "SUPERSHIFT,w,exec,${makoctl} restore"
+            ]
         )
         ++
         # Launcher
