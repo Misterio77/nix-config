1f717c44c0ee3a4b8644e5078a863ffa2f909089
Gabriel Fontes <hi@m7.rs>
2024-05-09 10:40:18 -0300

    feat(overlays): pkgs.stable as alias for pkgs.inputs.nixpkgs-stable

--- overlays/default.nix
+++ overlays/default.nix
@@ -24,6 +24,11 @@ in {
       inputs;
   };
 
+  # Adds pkgs.stable == inputs.nixpkgs-stable.legacyPackages.${pkgs.system}
+  stable = final: _: {
+    stable = inputs.nixpkgs-stable.legacyPackages.${final.system};
+  };
+
   # Adds my custom packages
   additions = final: prev:
     import ../pkgs {pkgs = final;}
