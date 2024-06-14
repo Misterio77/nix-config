ed78d1d100541689abb246a86d69a2e469a6a06c
Gabriel Fontes <hi@m7.rs>
2024-06-14 16:37:01 -0300

    home/electra: fix username

--- home/misterio/electra.nix
+++ home/misterio/electra.nix
@@ -4,5 +4,6 @@
     ./features/desktop/hyprland
   ];
   home.persistence."/persist/home/misterio" = lib.mkForce {};
+  home.username = "gabriel";
   wallpaper = pkgs.wallpapers.aenami-the-day-you-left;
 }
