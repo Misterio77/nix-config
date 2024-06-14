22cb1759aa6368ad2e431d6dc2371f3f6ea4787e
Gabriel Fontes <hi@m7.rs>
2024-06-14 16:16:49 -0300

    home/electra: try and fix

--- home/misterio/electra.nix
+++ home/misterio/electra.nix
@@ -1,7 +1,8 @@
-{pkgs, ...}: {
+{pkgs, lib, ...}: {
   imports = [
     ./global
     ./features/desktop/hyprland
   ];
+  persistence."/persist/home/misterio" = lib.mkForce {};
   wallpaper = pkgs.wallpapers.aenami-the-day-you-left;
 }
