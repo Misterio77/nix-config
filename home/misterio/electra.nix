276282c9aaf70dfb9797d87b064ab19e8a9c6ba7
Gabriel Fontes <hi@m7.rs>
2024-06-14 17:39:07 -0300

    home/electra: another attempt with nix gl

--- home/misterio/electra.nix
+++ home/misterio/electra.nix
@@ -1,13 +1,18 @@
-{pkgs, lib, ...}: {
+{pkgs, lib, ...}: let
+  nixGL = pkgs.inputs.nix-gl.nixGLIntel;
+in {
   imports = [
     ./global
     ./features/desktop/hyprland
   ];
   home.persistence."/persist/home/misterio" = lib.mkForce {};
   home.username = "gabriel";
+  home.packages = [nixGL];
 
   programs.fish.interactiveShellInit = /* fish */ ''
-    set -p LD_LIBRARY_PATH (${lib.getExe pkgs.inputs.nix-gl.nixGLIntel} printenv LD_LIBRARY_PATH)
+    set -p LD_LIBRARY_PATH (${lib.getExe nixGL} printenv LD_LIBRARY_PATH)
+    set -p LIBGL_DRIVERS_PATH (${lib.getExe nixGL} printenv LIBGL_DRIVERS_PATH)
+    set -p LIBVA_DRIVERS_PATH (${lib.getExe nixGL} printenv LIBVA_DRIVERS_PATH)
   '';
 
   wallpaper = pkgs.wallpapers.aenami-the-day-you-left;
