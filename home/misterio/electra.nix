3c1a50c2df69d3be5e659176abf37fed7f0e0049
Gabriel Fontes <hi@m7.rs>
2024-06-14 17:30:58 -0300

    home/electra: fix fish syntax

--- home/misterio/electra.nix
+++ home/misterio/electra.nix
@@ -7,7 +7,7 @@
   home.username = "gabriel";
 
   programs.fish.interactiveShellInit = /* fish */ ''
-    set -p LD_LIBRARY_PATH $(${lib.getExe pkgs.inputs.nix-gl.nixGLIntel} printenv LD_LIBRARY_PATH)
+    set -p LD_LIBRARY_PATH (${lib.getExe pkgs.inputs.nix-gl.nixGLIntel} printenv LD_LIBRARY_PATH)
   '';
 
   wallpaper = pkgs.wallpapers.aenami-the-day-you-left;
