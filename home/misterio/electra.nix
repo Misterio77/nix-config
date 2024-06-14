7c54bf78ffbe45f6ba0e1a2e7ece07246a8ca453
Gabriel Fontes <hi@m7.rs>
2024-06-14 17:26:46 -0300

    home/electra: append nixgl to ld_library_path

--- home/misterio/electra.nix
+++ home/misterio/electra.nix
@@ -5,6 +5,10 @@
   ];
   home.persistence."/persist/home/misterio" = lib.mkForce {};
   home.username = "gabriel";
-  home.packages = [pkgs.inputs.nix-gl.nixGLIntel];
+
+  programs.fish.interactiveShellInit = /* fish */ ''
+    set -p LD_LIBRARY_PATH $(${lib.getExe pkgs.inputs.nix-gl.nixGLIntel} printenv LD_LIBRARY_PATH)
+  '';
+
   wallpaper = pkgs.wallpapers.aenami-the-day-you-left;
 }
