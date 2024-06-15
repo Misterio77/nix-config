e4344cfd4e75d680e24dcba6a227c9c03cc39772
Gabriel Fontes <hi@m7.rs>
2024-06-15 16:00:21 -0300

    home/electra: add monitor and targets.genericLinux

--- home/misterio/electra.nix
+++ home/misterio/electra.nix
@@ -9,11 +9,21 @@ in {
   home.username = "gabriel";
   home.packages = [nixGL];
 
-  programs.fish.interactiveShellInit = /* fish */ ''
-    set -p LD_LIBRARY_PATH (${lib.getExe nixGL} printenv LD_LIBRARY_PATH)
-    set -p LIBGL_DRIVERS_PATH (${lib.getExe nixGL} printenv LIBGL_DRIVERS_PATH)
-    set -p LIBVA_DRIVERS_PATH (${lib.getExe nixGL} printenv LIBVA_DRIVERS_PATH)
-  '';
+  # programs.fish.interactiveShellInit = /* fish */ ''
+  #   set -p LD_LIBRARY_PATH (${lib.getExe nixGL} printenv LD_LIBRARY_PATH)
+  #   set -p LIBGL_DRIVERS_PATH (${lib.getExe nixGL} printenv LIBGL_DRIVERS_PATH)
+  #   set -p LIBVA_DRIVERS_PATH (${lib.getExe nixGL} printenv LIBVA_DRIVERS_PATH)
+  # '';
+
+  targets.genericLinux.enable = true;
+
+  monitors = [{
+    name = "eDP-1";
+    width = 1920;
+    height = 1080;
+    workspace = "1";
+    primary = true;
+  }];
 
   wallpaper = pkgs.wallpapers.aenami-the-day-you-left;
 }
