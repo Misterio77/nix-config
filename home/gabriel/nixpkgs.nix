016c41119d63c9bee3f90faf50723f501308bb65
Gabriel Fontes <hi@m7.rs>
2024-06-19 11:34:56 -0300

    feat(home/nix): add nix settings to hm too

--- home/gabriel/nixpkgs.nix
+++ home/gabriel/nixpkgs.nix
@@ -1,5 +1,40 @@
 # This file should be included when using hm standalone
-{outputs, ...}: {
+{
+  outputs,
+  lib,
+  inputs,
+  pkgs,
+  ...
+}: let
+  flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+in {
+  nix = {
+    package = pkgs.nixVersions.nix_2_22;
+
+    settings = {
+      substituters = lib.mkAfter ["https://cache.m7.rs"];
+      trusted-public-keys = ["cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg="];
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+      auto-optimise-store = lib.mkDefault true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "ca-derivations"
+      ];
+      warn-dirty = false;
+      system-features = [
+        "kvm"
+        "big-parallel"
+        "nixos-test"
+      ];
+      flake-registry = ""; # Disable global flake registry
+    };
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
   nixpkgs = {
     overlays = builtins.attrValues outputs.overlays;
     config = {
