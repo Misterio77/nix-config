bf397ca5007ff054048cd3973d08234ea9260f4b
Gabriel Fontes <hi@m7.rs>
2024-06-21 09:40:59 -0300

    fix(home/hyprland): fix screenshot animations

--- home/gabriel/features/desktop/hyprland/default.nix
+++ home/gabriel/features/desktop/hyprland/default.nix
@@ -95,13 +95,19 @@
         "suppressevent fullscreen, ${kdeconnect-pointer}"
       ];
       layerrule = [
+        "animation fade,hyprpicker"
+        "animation fade,selection"
+
         "animation fade,waybar"
         "blur,waybar"
         "ignorezero,waybar"
+
         "blur,notifications"
         "ignorezero,notifications"
+
         "blur,wofi"
         "ignorezero,wofi"
+
         "noanim,wallpaper"
       ];
 
