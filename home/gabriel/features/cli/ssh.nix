0dbfa943470214b7c32d235d5e0e9352404712b8
Gabriel Fontes <hi@m7.rs>
2024-06-24 16:46:37 -0300

    feat(home/ssh): cleanup sockets

--- home/gabriel/features/cli/ssh.nix
+++ home/gabriel/features/cli/ssh.nix
@@ -30,6 +30,9 @@ in {
           }
         ];
         setEnv.WAYLAND_DISPLAY = "/%d/.waypipe/display";
+        extraConfig = ''
+          StreamLocalBindUnlink yes
+        '';
       };
     };
   };
