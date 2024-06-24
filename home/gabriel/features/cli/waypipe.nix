9b7e78636247abcdb03bee6a6f3885c009e5a122
Gabriel Fontes <hi@m7.rs>
2024-06-24 18:33:01 -0300

    fix(home/waypipe): fix sleep

--- home/gabriel/features/cli/waypipe.nix
+++ home/gabriel/features/cli/waypipe.nix
@@ -15,7 +15,7 @@
       Service = {
         Type = "simple";
         ExecStartPre = "${lib.getExe' pkgs.coreutils "mkdir"} %h/.waypipe -p";
-        ExecStart = "${lib.getExe pkgs.waypipe} --socket %h/.waypipe/server.sock --login-shell --display %h/.waypipe/display server -- ${lib.getExe pkgs.sleep} infinity";
+        ExecStart = "${lib.getExe pkgs.waypipe} --socket %h/.waypipe/server.sock --login-shell --display %h/.waypipe/display server -- ${lib.getExe' pkgs.coreutils "sleep"} infinity";
         ExecStopPost = "${lib.getExe' pkgs.coreutils "rm"} -f %h/.waypipe/server.sock %h/.waypipe/display";
       };
       Install.WantedBy = ["default.target"];
