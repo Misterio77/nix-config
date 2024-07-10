9124b037a9101a940319ddd998dcfbb52c2d31b1
Gabriel Fontes <hi@m7.rs>
2024-07-10 09:40:35 -0300

    fix(home/waypipe): typo

--- home/gabriel/features/desktop/common/wayland-wm/waypipe.nix
+++ home/gabriel/features/desktop/common/wayland-wm/waypipe.nix
@@ -16,7 +16,7 @@
         Type = "simple";
         ExecStartPre = "${lib.getExe' pkgs.coreutils "mkdir"} %h/.waypipe -p";
         ExecStart = "${lib.getExe pkgs.waypipe} --socket %h/.waypipe/server.sock --title-prefix '[%H] ' --login-shell --display wayland-waypipe server -- ${lib.getExe' pkgs.coreutils "sleep"} infinity";
-        ExecStopPost = "${lib.getExe' pkgs.coreutils "rm"} -f %h/.waypipe/server.sock $t/wayland-waypipe";
+        ExecStopPost = "${lib.getExe' pkgs.coreutils "rm"} -f %h/.waypipe/server.sock %t/wayland-waypipe";
       };
       Install.WantedBy = ["default.target"];
     };
