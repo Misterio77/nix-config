25b97ff901218960e85b144415e902e24b7fb68d
Gabriel Fontes <hi@m7.rs>
2024-07-22 14:33:38 -0300

    feat(home/firefox): disable more telemetry

--- home/gabriel/features/desktop/common/firefox.nix
+++ home/gabriel/features/desktop/common/firefox.nix
@@ -1,4 +1,8 @@
-{pkgs, lib, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.browserpass.enable = true;
   programs.firefox = {
     enable = true;
@@ -35,7 +39,6 @@
         "browser.startup.homepage_override.mstone" = "ignore";
         "browser.uitour.enabled" = false;
         "startup.homepage_override_url" = "";
-        "toolkit.telemetry.reportingpolicy.firstRun" = false;
         "trailhead.firstrun.didSeeAboutWelcome" = true;
         "browser.bookmarks.restore_default_bookmarks" = false;
         "browser.bookmarks.addedImportButton" = true;
@@ -64,6 +67,30 @@
 
         # Disable some telemetry
         "app.shield.optoutstudies.enabled" = false;
+        "browser.discovery.enabled" = false;
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.newtabpage.activity-stream.telemetry" = false;
+        "browser.ping-centre.telemetry" = false;
+        "datareporting.healthreport.service.enabled" = false;
+        "datareporting.healthreport.uploadEnabled" = false;
+        "datareporting.policy.dataSubmissionEnabled" = false;
+        "datareporting.sessions.current.clean" = true;
+        "devtools.onboarding.telemetry.logged" = false;
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "toolkit.telemetry.hybridContent.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.prompted" = 2;
+        "toolkit.telemetry.rejected" = true;
+        "toolkit.telemetry.reportingpolicy.firstRun" = false;
+        "toolkit.telemetry.server" = "";
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.unifiedIsOptIn" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
+
         # Disable fx accounts
         "identity.fxaccounts.enabled" = false;
         # Disable "save password" prompt
@@ -116,7 +143,7 @@
             "currentVersion": 20,
             "newElementCount": 5
           }
-'';
+        '';
       };
     };
   };
