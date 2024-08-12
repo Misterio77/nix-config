fb63246ef7d16ada72c0571079282721d6d8bc3b
Gabriel Fontes <hi@m7.rs>
2024-08-12 16:24:43 -0300

    refactor(home/qt): we actually wont need the symlinks

--- home/gabriel/features/desktop/common/qt.nix
+++ home/gabriel/features/desktop/common/qt.nix
@@ -4,14 +4,15 @@
     platformTheme = {
       name = "gtk3";
       package = [
+        # QT 5
         (pkgs.libsForQt5.qtstyleplugins.overrideAttrs (old: {
+          # Make qtstyleplugins' gtk2 platform theme activate if QT_QPA_PLATFORMTHEME=gtk3
           patches = (old.patches or []) ++ [./qtstyleplugins-gtk3-key.patch];
-          postInstall = (old.postInstall or "") + ''
-            ln -s $out/${pkgs.qt5.qtbase.qtPluginPrefix}/platformthemes/libqgtk{2,3}.so
-            ln -s $out/${pkgs.qt5.qtbase.qtPluginPrefix}/styles/libqgtk{2,3}style.so
-          '';
         }))
+        # QT 6
         (pkgs.qt6.qtbase.override {
+          # Make qtbase's gtk3 platform theme read dark/light status from xdp.
+          # This is specially important as it's what qutebrowser reads to determine prefers-color-scheme.
           # https://codereview.qt-project.org/c/qt/qtbase/+/547252
           patches = [./qtbase-gtk3-xdp.patch];
           qttranslations = null;
