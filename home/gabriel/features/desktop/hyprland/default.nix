1a48f207f99dce77660298cd59bc0583753272f1
Gabriel Fontes <gabriel.fontes@luizalabs.com>
2024-09-04 09:36:52 -0300

    fix(home/swaylock): workaround --grace not working
    Ref: https://github.com/jirutka/swaylock-effects/issues/68

--- home/gabriel/features/desktop/hyprland/default.nix
+++ home/gabriel/features/desktop/hyprland/default.nix
@@ -247,8 +247,8 @@ in {
             swaylock = lib.getExe config.programs.swaylock.package;
           in
             lib.optionals config.programs.swaylock.enable [
-              "SUPER,backspace,exec,${swaylock} -S --grace 2"
-              "SUPER,XF86Calculator,exec,${swaylock} -S --grace 2"
+              "SUPER,backspace,exec,${swaylock} -S --grace 2 --grace-no-mouse"
+              "SUPER,XF86Calculator,exec,${swaylock} -S --grace 2 --grace-no-mouse"
             ]
         )
         ++
