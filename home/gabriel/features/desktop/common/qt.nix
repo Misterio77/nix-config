c4914aa06a95f26b6a5b2866a836169460f20b4d
Gabriel Fontes <hi@m7.rs>
2024-11-12 08:43:43 -0300

    fix(home/qt): remove qgtk3theme patch
    It's in upstream now yay

--- home/gabriel/features/desktop/common/qt.nix
+++ home/gabriel/features/desktop/common/qt.nix
@@ -10,13 +10,7 @@
           patches = (old.patches or []) ++ [./qtstyleplugins-gtk3-key.patch];
         }))
         # QT 6
-        (pkgs.qt6.qtbase.override {
-          # Make qtbase's gtk3 platform theme read dark/light status from xdp.
-          # This is specially important as it's what qutebrowser reads to determine prefers-color-scheme.
-          # https://codereview.qt-project.org/c/qt/qtbase/+/547252
-          patches = [./qtbase-gtk3-xdp.patch];
-          qttranslations = null;
-        })
+        (pkgs.qt6.qtbase)
       ];
     };
   };
