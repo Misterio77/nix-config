4a743355b7acb30f6edf1e26e964fbc9cf7d2662
Gabriel Fontes <hi@m7.rs>
2024-11-26 09:57:49 -0300

    chore(flake): update lock
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/76612b17c0ce71689921ca12d9ffdc9c23ce40b2?narHash=sha256-IigrKK3vYRpUu%2BHEjPL/phrfh7Ox881er1UEsZvw9Q4%3D' (2024-11-09)
      → 'github:nixos/nixpkgs/23e89b7da85c3640bbc2173fe04f4bd114342367?narHash=sha256-y/MEyuJ5oBWrWAic/14LaIr/u5E0wRVzyYsouYY3W6w%3D' (2024-11-19)
    • Updated input 'nixpkgs-stable':
        'github:nixos/nixpkgs/9256f7c71a195ebe7a218043d9f93390d49e6884?narHash=sha256-q2yjIWFFcTzp5REWQUOU9L6kHdCDmFDpqeix86SOvDc%3D' (2024-11-10)
      → 'github:nixos/nixpkgs/e8c38b73aeb218e27163376a2d617e61a2ad9b59?narHash=sha256-df3dJApLPhd11AlueuoN0Q4fHo/hagP75LlM5K1sz9g%3D' (2024-11-16)
    • Updated input 'firefox-addons':
        'gitlab:rycee/nur-expressions/85c8db7e7e901f656072b01f89c8fffcab26bfa6?dir=pkgs/firefox-addons&narHash=sha256-YC%2BFPREQqPF2HNSu1PkvVps5mBzexxvCV6NXfTf/kQI%3D' (2024-11-11)
      → 'gitlab:rycee/nur-expressions/9b7d4fb4739c0bf808d31e2b66b337f1234e4447?dir=pkgs/firefox-addons&narHash=sha256-VhS%2B81/eTDiin5ULZLWvqI%2B9ggI%2BqXNUSvi3dIQfBX4%3D' (2024-11-26)
    • Updated input 'hardware':
        'github:nixos/nixos-hardware/184687ae1a3139faa4746168baf071f60d0310c8?narHash=sha256-0ctfVp27ingWtY7dbP5%2BQpSQ98HaOZleU0teyHQUAw0%3D' (2024-11-11)
      → 'github:nixos/nixos-hardware/45348ad6fb8ac0e8415f6e5e96efe47dd7f39405?narHash=sha256-kF6rDeCshoCgmQz%2B7uiuPdREVFuzhIorGOoPXMalL2U%3D' (2024-11-24)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/60bb110917844d354f3c18e05450606a435d2d10?narHash=sha256-NjavpgE9/bMe/ABvZpyHIUeYF1mqR5lhaep3wB79ucs%3D' (2024-11-10)
      → 'github:nix-community/home-manager/a9953635d7f34e7358d5189751110f87e3ac17da?narHash=sha256-GUffLwzawz5WRVfWaWCg78n/HrBJrOG7QadFY6rtV8A%3D' (2024-11-24)
    • Updated input 'nix-gaming':
        'github:fufexan/nix-gaming/3bba155a6dfe6a1922d3cf39828576ef99aacb6a?narHash=sha256-0dqCIOQAz9iiA6Km%2B/9FJPohfbN/msLAytdOGZEWOog%3D' (2024-11-11)
      → 'github:fufexan/nix-gaming/18b8f0796f3e9dd8b9d9f262a56f2914a0586a22?narHash=sha256-suhk41NhdsnN3aa7gpsr8KnYJtbaLeZBJMB2vqWbkzA%3D' (2024-11-25)
    • Updated input 'sops-nix':
        'github:mic92/sops-nix/f1675e3b0e1e663a4af49be67ecbc9e749f85eb7?narHash=sha256-jR8i6nFLmSmm0cIoeRQ8Q4EBARa3oGaAtEER/OMMxus%3D' (2024-11-10)
      → 'github:mic92/sops-nix/3433ea14fbd9e6671d0ff0dd45ed15ee4c156ffa?narHash=sha256-xtt95%2Bc7OUMoqZf4OvA/7AemiH3aVuWHQbErYQoPwFk%3D' (2024-11-25)

--- flake.nix
+++ flake.nix
@@ -29,7 +29,6 @@
     sops-nix = {
       url = "github:mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
     };
     nixos-mailserver = {
       url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
