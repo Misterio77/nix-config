388df1538c8a99792628afd0d84bd38e989be5ed
Gabriel Fontes <hi@m7.rs>
2024-12-13 12:07:49 -0300

    fix(home/games/star-citizen): enable gamescope, enable umu

--- home/gabriel/features/games/star-citizen.nix
+++ home/gabriel/features/games/star-citizen.nix
@@ -1,5 +1,23 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  monitor = lib.head (lib.filter (m: m.primary) config.monitors);
+in {
   home.packages = [
-    (pkgs.inputs.nix-gaming.star-citizen.override {disableEac = false;})
+    (pkgs.inputs.nix-gaming.star-citizen.override {
+      disableEac = false;
+      useUmu = true;
+      gameScopeEnable = true;
+      gameScopeArgs = [
+        "--force-grab-cursor"
+        "--output-width ${toString monitor.width}"
+        "--output-height ${toString monitor.height}"
+        "--framerate-limit ${toString monitor.refreshRate}"
+        "--prefer-output ${monitor.name}"
+      ];
+    })
   ];
 }
