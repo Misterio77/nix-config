d543cf40fb5478ddf2f0508ca5e8cccb7e9c95b0
Gabriel Fontes <hi@m7.rs>
2024-12-19 16:49:13 -0300

    feat(home/jujutsu): go back to default jujutsu diffeditor

--- home/gabriel/features/cli/jujutsu.nix
+++ home/gabriel/features/cli/jujutsu.nix
@@ -7,9 +7,6 @@
         email = config.programs.git.userEmail;
       };
       ui = {
-        diff-editor = lib.mkIf config.programs.neovim.enable [
-          "nvim" "-c" "DiffEditor $left $right $output"
-        ];
         pager = "less -FRX";
       };
       signing = let
