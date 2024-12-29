a59627d2d22a3f03079eec703e6710b1d80cb164
Gabriel Fontes <hi@m7.rs>
2024-12-29 14:57:26 -0300

    fix(celaeno/minecraft): enable bungeeord in lobby spigot.yml

--- hosts/celaeno/services/minecraft/servers/lobby/default.nix
+++ hosts/celaeno/services/minecraft/servers/lobby/default.nix
@@ -38,6 +38,10 @@
         };
       };
       "spigot.yml".value = {
+        settings = {
+          bungeecord = true;
+          restart-on-crash = false;
+        };
         messages = {
           whitelist = "Você não está na whitelist!";
           unknown-command = "Comando desconhecido.";
