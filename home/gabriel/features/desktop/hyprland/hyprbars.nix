31350ebf6ee856f8b904cc546dc30e214ced9d87
Gabriel Fontes <hi@m7.rs>
2025-01-13 10:22:48 -0300

    fix(home/hyprland): update windowrules for 0.46.0
    Regex matches now are for an entire line, so remove anchors and wildcards where nescessary.

--- home/gabriel/features/desktop/hyprland/hyprbars.nix
+++ home/gabriel/features/desktop/hyprland/hyprbars.nix
@@ -62,11 +62,11 @@ in {
         "plugin:hyprbars:bar_color ${rgba config.colorscheme.colors.primary "ee"}, focus:1"
         "plugin:hyprbars:title_color ${rgb config.colorscheme.colors.on_primary}, focus:1"
       ] ++ (lib.flatten (lib.mapAttrsToList (name: colors: [
-        "plugin:hyprbars:bar_color ${rgba colors.primary_container "dd"}, title:^(\\[${name}\\])"
-        "plugin:hyprbars:title_color ${rgb colors.on_primary_container}, title:^(\\[${name}\\])"
+        "plugin:hyprbars:bar_color ${rgba colors.primary_container "dd"}, title:\\[${name}\\].*"
+        "plugin:hyprbars:title_color ${rgb colors.on_primary_container}, title:\\[${name}\\].*"
 
-        "plugin:hyprbars:bar_color ${rgba colors.primary "ee"}, title:^(\\[${name}\\]), focus:1"
-        "plugin:hyprbars:title_color ${rgb colors.on_primary}, title:^(\\[${name}\\]), focus:1"
+        "plugin:hyprbars:bar_color ${rgba colors.primary "ee"}, title:\\[${name}\\].*, focus:1"
+        "plugin:hyprbars:title_color ${rgb colors.on_primary}, title:\\[${name}\\].*, focus:1"
       ]) remoteColorschemes));
     };
   };
