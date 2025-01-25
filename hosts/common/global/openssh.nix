c82863f453ffa766fc50b407f99e609c8e74bf30
Gabriel Fontes <hi@m7.rs>
2025-01-25 15:16:35 -0300

    fix(hots/ssh): temporarily enable password auth

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -14,7 +14,7 @@ in {
     enable = true;
     settings = {
       # Harden
-      PasswordAuthentication = false;
+      PasswordAuthentication = true;
       PermitRootLogin = "no";
 
       # Automatically remove stale sockets
