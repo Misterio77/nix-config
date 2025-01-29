c74dda058c9dc4bac4918f4bf79fc37080828ec0
Gabriel Fontes <hi@m7.rs>
2025-01-29 10:18:13 -0300

    chore(hosts/auto-upgrade): change to every 10 minutes

--- hosts/common/global/auto-upgrade.nix
+++ hosts/common/global/auto-upgrade.nix
@@ -2,7 +2,7 @@
   system.hydraAutoUpgrade = {
     # Only enable if not dirty
     enable = inputs.self ? rev;
-    dates = "minutely";
+    dates = "*:0/10"; # Every 10 minutes
     instance = "https://hydra.m7.rs";
     project = "nix-config";
     jobset = "main";
