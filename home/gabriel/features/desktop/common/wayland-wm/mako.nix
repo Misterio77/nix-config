95ef53287ebc539dfe5d83d6dea32a5806bd9562
Gabriel Fontes <hi@m7.rs>
2025-05-16 10:34:03 +0200

    fix(home/mako): migrate to settings

--- home/gabriel/features/desktop/common/wayland-wm/mako.nix
+++ home/gabriel/features/desktop/common/wayland-wm/mako.nix
@@ -3,23 +3,23 @@
 in {
   services.mako = {
     enable = true;
-    iconPath =
-      if mode == "dark"
-      then "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark"
-      else "${config.gtk.iconTheme.package}/share/icons/Papirus-Light";
-    font = "${config.fontProfiles.regular.name} ${toString config.fontProfiles.regular.size}";
-    padding = "10,20";
-    anchor = "top-center";
-    width = 400;
-    height = 150;
-    borderSize = 2;
-    defaultTimeout = 12000;
-    backgroundColor = "${colors.surface}dd";
-    borderColor = "${colors.secondary}dd";
-    textColor = "${colors.on_surface}dd";
-    layer = "overlay";
-    extraConfig = ''
-      max-history=50
-    '';
+    settings = {
+      icon-path =
+        if mode == "dark"
+        then "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark"
+        else "${config.gtk.iconTheme.package}/share/icons/Papirus-Light";
+      font = "${config.fontProfiles.regular.name} ${toString config.fontProfiles.regular.size}";
+      padding = "10,20";
+      anchor = "top-center";
+      width = 400;
+      height = 150;
+      border-size = 2;
+      default-timeout = 12000;
+      background-color = "${colors.surface}dd";
+      border-color = "${colors.secondary}dd";
+      text-color = "${colors.on_surface}dd";
+      layer = "overlay";
+      max-history = 50;
+    };
   };
 }
