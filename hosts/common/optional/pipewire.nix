8ac5f9b51710389f2601f379b22495d51535b906
Gabriel Fontes <hi@m7.rs>
2025-05-16 10:36:16 +0200

    fix(hosts): hardware.pipewire -> services.pipewire

--- hosts/common/optional/pipewire.nix
+++ hosts/common/optional/pipewire.nix
@@ -1,6 +1,6 @@
 {
   security.rtkit.enable = true;
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
