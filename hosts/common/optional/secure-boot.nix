d632dc6d3b543aafb94bedd63489d17b1c986910
Gabriel Fontes <hi@m7.rs>
2025-05-16 19:20:33 +0200

    docs: document secure boot setup

--- hosts/common/optional/secure-boot.nix
+++ hosts/common/optional/secure-boot.nix
@@ -1,3 +1,16 @@
+# Manual steps:
+# 1. bootctl status
+# 2. Make sure you have BIOS password and disk encryption
+# 3. sbctl create-keys
+# 4. Put secure boot into setup mode
+# 5. sbctl enroll-keys --microsoft
+# 6. Enable secure boot
+# 7. systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=0+2+7+12+13+14+15:sha256=0000000000000000000000000000000000000000000000000000000000000000 --wipe-slot=tpm2 <DEVICE>
+#      Explanation:
+#      - PCR7: Secure boot is on
+#      - PCR0+2: UEFI integrity
+#      - PCR12+13+14: Boot loader integrity
+#      - PCR15: No LUKS partition has been opened yet
 {pkgs, inputs, lib, ...}: {
   imports = [
     inputs.lanzaboote.nixosModules.lanzaboote
