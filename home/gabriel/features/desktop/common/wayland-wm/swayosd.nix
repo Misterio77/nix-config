09b4bb72e96634a51317190f1adaa3129f8a2c20
Gabriel Fontes <hi@m7.rs>
2025-05-22 19:24:32 +0200

    feat(home/swayosd): theme it

--- home/gabriel/features/desktop/common/wayland-wm/swayosd.nix
+++ home/gabriel/features/desktop/common/wayland-wm/swayosd.nix
@@ -1,3 +1,58 @@
-{
-  services.swayosd.enable = true;
+{pkgs, config, ...}: let
+  inherit (config.colorscheme) colors;
+in {
+  services.swayosd = {
+    enable = true;
+    stylePath = pkgs.writeText "style.css" ''
+      window {
+        padding: 0 1em;
+        border: 10em;
+        border-radius: 10em;
+        background-color: ${colors.surface};
+        opacity: 0.9;
+      }
+      #container {
+        margin: 1em;
+      }
+
+      image {
+        color: ${colors.primary};
+        opacity: 0.9;
+      }
+      image:disabled {
+        color: ${colors.on_surface};
+        opacity: 0.8;
+      }
+      label {
+        color: ${colors.on_surface};
+        opacity: 1;
+      }
+
+      progress {
+        min-height: inherit;
+        border-radius: inherit;
+        border: none;
+        background-color: ${colors.on_surface};
+        opacity: 0.9;
+      }
+      progressbar {
+        min-height: 0.5em;
+        border-radius: 100em;
+        background-color: transparent;
+        border: none;
+        opacity: 0.9;
+      }
+      progressbar:disabled {
+        opacity: 0.5;
+      }
+
+      trough {
+        min-height: inherit;
+        border-radius: inherit;
+        border: none;
+        background-color: ${colors.on_surface};
+        opacity: 1;
+      }
+    '';
+  };
 }
