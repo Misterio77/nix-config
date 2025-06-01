224f0df1e77ecd6c4a2177ac897bad8c8d42fe84
Gabriel Fontes <hi@m7.rs>
2025-06-01 05:15:16 +0200

    fix(taygeta/minecraft/proxy): try and fix forge velocity

--- hosts/taygeta/services/minecraft/servers/proxy/default.nix
+++ hosts/taygeta/services/minecraft/servers/proxy/default.nix
@@ -64,26 +64,6 @@ in {
         };
       };
       "lang/messages.properties" = ./messages.properties;
-      "plugins/ambassador/Ambassador.toml".value = {
-        config-version = "2.1";
-        bypass-registry-checks = true;
-        enable-kick-reset = true;
-        reconnect-message = "&ePor favor, reconecte.";
-      };
-    };
-    symlinks = {
-      "plugins/OwoVelocityPlugin.jar" = pkgs.fetchurl rec {
-        pname = "OwoVelocityPlugin";
-        version = "0.1.2";
-        url = "https://github.com/wisp-forest/owo-velocity-plugin/releases/download/${version}/${pname}.jar";
-        hash = "sha256-aiAlYdJV2tCxaCMWv9S0Opn29aMGHVyPiJ00ePe1CDw=";
-      };
-      "plugins/Ambassador-Velocity.jar" = pkgs.fetchurl rec {
-        pname = "Ambassador";
-        version = "1.4.5";
-        url = "https://github.com/adde0109/Ambassador/releases/download/v${version}/Ambassador-Velocity-${version}-all.jar";
-        hash = "sha256-fFemScOUhnLL7zWjuqj3OwRqxQnqj/pu4wCIkNNvLBc=";
-      };
     };
   };
 }
