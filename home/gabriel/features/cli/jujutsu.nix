0d0e64f1fbb113f6d92cf5163ff12cb41f9646a3
Gabriel Fontes <hi@m7.rs>
2025-06-02 17:09:52 +0200

    feat(home/cli/jujutsu): add tug alias

--- home/gabriel/features/cli/jujutsu.nix
+++ home/gabriel/features/cli/jujutsu.nix
@@ -17,6 +17,13 @@
         behaviour = if gitCfg.commit.gpgSign then "own" else "never";
         key = gitCfg.user.signing.key;
       };
+      revset-aliases = {
+        "closest_bookmark(to)" = "heads(::to & bookmarks())";
+      };
+      aliases = {
+        # Advances closest bookmark to parent commit
+        tug = ["bookmark" "move" "--from" "closest_bookmark(@-)" "--to" "@-"];
+      };
       template-aliases = {
         "gerrit_change_id(change_id)" = ''
           "Id0000000" ++ change_id.normal_hex()
