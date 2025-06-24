0ae5f72922465c66c481c078a7ed726d87e09a95
Gabriel Fontes <hi@m7.rs>
2025-06-24 09:48:16 +0200

    chore(home/steam): remove game dir persistence
    It's kinda glitchy in later versions of steam-runtime, for some reason. So let's just rely on cloud saves for now.

--- home/gabriel/features/games/steam.nix
+++ home/gabriel/features/games/steam.nix
@@ -58,16 +58,6 @@ in {
     "/persist/${config.home.homeDirectory}" = {
       allowOther = true;
       directories = [
-        {
-          directory = ".factorio";
-          method = "bindfs";
-        }
-        ".config/Hero_Siege"
-        ".config/unity3d/Berserk Games/Tabletop Simulator"
-        ".config/unity3d/IronGate/Valheim"
-        ".local/share/Tabletop Simulator"
-        ".local/share/Paradox Interactive"
-        ".paradoxlauncher"
         ".local/share/Steam"
       ];
     };
