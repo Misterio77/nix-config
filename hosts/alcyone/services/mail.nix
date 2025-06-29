73dea439cd6ac61db6f59e5619edbbf4c7bc28ec
Gabriel Fontes <hi@m7.rs>
2025-06-29 15:35:53 +0200

    fix(alcyone/mailserver): init stateVersion at 1

--- hosts/alcyone/services/mail.nix
+++ hosts/alcyone/services/mail.nix
@@ -11,6 +11,7 @@
   services.dovecot2.sieve.extensions = ["fileinto"];
 
   mailserver = rec {
+    stateVersion = 1;
     enable = true;
     fqdn = "mail.m7.rs";
     sendingFqdn = "alcyone.m7.rs";
