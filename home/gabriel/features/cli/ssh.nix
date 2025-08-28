0f53bdc65f94c2f0efee8a2ef9f380f58d5d602b
Gabriel Fontes <hi@m7.rs>
2025-08-28 14:39:13 -0300

    fix(home/ssh): don't try to persist known hosts

--- home/gabriel/features/cli/ssh.nix
+++ home/gabriel/features/cli/ssh.nix
@@ -8,19 +8,9 @@
   homeConfigs = map (n: lib.last (lib.splitString "@" n)) (builtins.attrNames outputs.homeConfigurations);
   hostnames = lib.unique (homeConfigs ++ nixosConfigs);
 in {
-  # Persisting known_hosts with impermance is wonky, as SSH sometimes
-  # overwrites it. My workaround is to make a known_hosts.d directory instead,
-  # which is persisted.
-  home.persistence = {
-    "/persist".directories = [
-      ".ssh/known_hosts.d"
-    ];
-  };
-
   programs.ssh = {
     enable = true;
     # See above
-    userKnownHostsFile = "${config.home.homeDirectory}/.ssh/known_hosts.d/hosts";
     matchBlocks = {
       net = {
         host = lib.concatStringsSep " " (lib.flatten (map (host: [
@@ -47,9 +37,4 @@ in {
       };
     };
   };
-
-  # Compatibility with programs that don't respect SSH configurations (e.g. jujutsu's libssh2)
-  systemd.user.tmpfiles.rules = [
-    "L ${config.home.homeDirectory}/.ssh/known_hosts - - - - ${config.programs.ssh.userKnownHostsFile}"
-  ];
 }
