383221881116c90578468cd02a68bd5f06921d7b
Gabriel Fontes <hi@m7.rs>
2025-09-03 10:58:11 -0300

    fix(home/oama): use access_type offline
    This hopefully avoids the re-auth

--- home/gabriel/features/productivity/oama.nix
+++ home/gabriel/features/productivity/oama.nix
@@ -9,6 +9,7 @@ in {
         client_id_cmd = "${pass} oama/google_client_id | head -1";
         client_secret_cmd = "${pass} oama/google_client_secret | head -1";
         auth_scope = "https://mail.google.com/ https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/contacts";
+        access_type = "offline";
       };
     };
   };
