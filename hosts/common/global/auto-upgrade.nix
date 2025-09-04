d708522d94e21be042855d4e231ea46420dabfc3
Gabriel Fontes <hi@m7.rs>
2025-09-04 20:01:17 -0300

    fix(auto-upgrade): disable timer instead of nuking service when dirty

--- hosts/common/global/auto-upgrade.nix
+++ hosts/common/global/auto-upgrade.nix
@@ -1,7 +1,6 @@
 {inputs, config, ...}: {
   system.hydraAutoUpgrade = {
-    # Only enable if not dirty
-    enable = inputs.self ? rev;
+    enable = true;
     dates = "*:0/10"; # Every 10 minutes
     instance = "https://hydra.m7.rs";
     project = "nix-config";
@@ -9,4 +8,6 @@
     job = "hosts.${config.networking.hostName}";
     oldFlakeRef = "self";
   };
+  # Disable timer if flake is dirty
+  systemd.timers.nixos-upgrade.enable = inputs.self ? rev;
 }
