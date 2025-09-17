54c982f8c3dad6ef44530534538b5056b3b5e075
Gabriel Fontes <hi@m7.rs>
2025-09-17 17:31:28 -0300

    fix(merope/lidarr): fix typo

--- hosts/merope/services/media/lidarr.nix
+++ hosts/merope/services/media/lidarr.nix
@@ -1,13 +1,16 @@
 {config, ...}: {
   services.lidarr = {
     enable = true;
+    settings = {
+      server.port = 8686;
+    };
   };
 
   environment.persistence = {
     "/persist".directories = [{
       directory = "/var/lib/lidarr";
-      user = config.services.user;
-      group = config.services.group;
+      user = config.services.lidarr.user;
+      group = config.services.lidarr.group;
     }];
   };
 }
