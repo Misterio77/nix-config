55fb0493531b2bb4ed64330e6a70892277aa8f94
Gabriel Fontes <hi@m7.rs>
2025-09-17 21:31:58 -0300

    fix(merope/lidarr): add lidarr to deluge group, to ensure hard linking can happen

--- hosts/merope/services/media/lidarr.nix
+++ hosts/merope/services/media/lidarr.nix
@@ -6,6 +6,10 @@
     };
   };
 
+  # Add lidarr to deluge's group
+  # Make sure lidarr can hard-link deluge-owned files
+  users.users.lidarr.extraGroups = [config.users.users.deluge.group];
+
   environment.persistence = {
     "/persist".directories = [{
       directory = "/var/lib/lidarr";
