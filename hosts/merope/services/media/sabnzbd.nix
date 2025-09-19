24775631772749975d6e751523a6d74ed138ac47
Gabriel Fontes <hi@m7.rs>
2025-09-19 18:38:12 -0300

    fix(merope/asbnzbd): save results in 770 instead of 700

--- hosts/merope/services/media/sabnzbd.nix
+++ hosts/merope/services/media/sabnzbd.nix
@@ -15,6 +15,7 @@
       complete_dir = /var/lib/sabnzbd/complete
       log_dir = /var/lib/sabnzbd/logs
       admin_dir = /var/lib/sabnzbd/admin
+      permissions = 770
 
       [categories]
       [[music]]
