9d4a78695a33693e73540e69d00968ec6ce63341
Gabriel Fontes <hi@m7.rs>
2025-09-19 23:04:25 -0300

    feat(merope/radarr): init

---
+++ hosts/merope/services/media/radarr.nix
@@ -0,0 +1,32 @@
+{config, ...}: {
+  services.radarr = {
+    enable = true;
+    settings = {
+      server.port = 8687;
+    };
+  };
+
+  # Add radarr to deluge's and nzbget's groups
+  # Make sure radarr can hard-link their files
+  users.users.radarr.extraGroups = [
+    config.users.users.deluge.group
+    config.services.sabnzbd.group
+  ];
+
+  environment.persistence = {
+    "/persist".directories = [
+      {
+        directory = config.services.radarr.dataDir;
+        user = config.services.radarr.user;
+        group = config.services.radarr.group;
+        mode = "0700";
+      }
+      {
+        directory = "/srv/media/movies";
+        user = config.services.radarr.user;
+        group = config.services.radarr.group;
+        mode = "0755";
+      }
+    ];
+  };
+}
