00f551dc05f3e84c368b737f72c95eed9697a233
Gabriel Fontes <hi@m7.rs>
2025-09-21 02:29:51 -0300

    fix(merope/prowlarr): disable dynamicuser

--- hosts/merope/services/media/prowlarr.nix
+++ hosts/merope/services/media/prowlarr.nix
@@ -1,4 +1,4 @@
-{config, ...}: {
+{config, lib, ...}: {
   services.prowlarr = {
     enable = true;
     settings = {
@@ -8,7 +8,19 @@
 
   environment.persistence = {
     "/persist".directories = [{
-      directory = "/var/lib/private/prowlarr";
+      directory = "/var/lib/prowlarr";
     }];
+  };
+
+  # Disable DynamicUser
+  systemd.services.prowlarr.serviceConfig = {
+    DynamicUser = lib.mkForce false;
+    User = lib.mkForce "prowlarr";
+    Group = lib.mkForce "prowlarr";
+  };
+  users.users.prowlarr = {
+    home = "/var/lib/prowlarr";
+    group = "prowlarr";
+    isSystemUser = true;
   };
 }
