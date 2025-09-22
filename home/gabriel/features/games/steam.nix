1afdaae2749a06e5aa235e72c167c9048153aa2c
Gabriel Fontes <hi@m7.rs>
2025-09-22 19:02:41 -0300

    fix: make "Exit to Desktop" work

--- home/gabriel/features/games/steam.nix
+++ home/gabriel/features/games/steam.nix
@@ -1,60 +1,18 @@
-{
-  pkgs,
-  lib,
-  config,
-  ...
-}: let
+{pkgs, ...}: let
   steam-with-pkgs = pkgs.steam.override {
-    extraPkgs = pkgs:
-      with pkgs; [
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXinerama
-        xorg.libXScrnSaver
-        libpng
-        libpulseaudio
-        libvorbis
-        stdenv.cc.cc.lib
-        libkrb5
-        keyutils
-        gamescope
-      ];
+    extraPkgs = pkgs: [
+      (pkgs.writeShellScriptBin "steamos-session-select" ''
+        /usr/bin/env steam -shutdown
+      '')
+      pkgs.gamescope
+    ];
   };
-
-  monitor = lib.head (lib.filter (m: m.primary) config.monitors);
-  steam-session = let
-    gamescope = lib.concatStringsSep " " [
-      (lib.getExe pkgs.gamescope)
-      "--output-width ${toString monitor.width}"
-      "--output-height ${toString monitor.height}"
-      "--framerate-limit ${toString monitor.refreshRate}"
-      "--prefer-output ${monitor.name}"
-      "--adaptive-sync"
-      "--expose-wayland"
-      "--hdr-enabled"
-      "--steam"
-    ];
-    steam = lib.concatStringsSep " " [
-      "steam"
-      "steam://open/bigpicture"
-    ];
-  in
-    (pkgs.writeTextDir "share/wayland-sessions/steam-session.desktop" # ini
-
-    ''
-      [Desktop Entry]
-      Name=Steam Session
-      Exec=${gamescope} -- ${steam}
-      Type=Application
-    '').overrideAttrs (_: {passthru.providedSessions = ["steam-session"];});
 in {
   home.packages = [
     steam-with-pkgs
     pkgs.gamescope
     pkgs.protontricks
   ];
-
-  home.exportedSessionPackages = [steam-session];
 
   home.persistence = {
     "/persist".directories = [
--- hosts/pleione/steam.nix
+++ hosts/pleione/steam.nix
@@ -1,8 +1,15 @@
-{
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
     gamescopeSession = {
       enable = true;
+      steamArgs = ["-tenfoot" "-pipewire-dmabuf" "-steamos3"];
     };
+    extraPackages = [
+      # Command executed when using "Exit to Desktop" in '-steamos3' mode
+      (pkgs.writeShellScriptBin "steamos-session-select" ''
+        /usr/bin/env steam -shutdown
+      '')
+    ];
   };
 }
