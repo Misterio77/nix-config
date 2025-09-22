681074f1f0891a4796b224f531a4d80b410ab414
Gabriel Fontes <hi@m7.rs>
2025-09-22 19:49:06 -0300

    feat(pleione/jellyfin): lockdown a bit, but disable kiosk mode (it's hard to exit from)

--- hosts/pleione/jellyfin-kiosk.nix
+++ hosts/pleione/jellyfin-kiosk.nix
@@ -1,10 +1,49 @@
 {pkgs, lib, ...}: let
+  firefox = url: pkgs.wrapFirefox pkgs.firefox-unwrapped {
+    extraPolicies = {
+      AutofillAddressEnabled = false;
+      AutofillCreditCardEnabled = false;
+      BlockAboutAddons = true;
+      BlockAboutConfig = true;
+      BlockAboutProfiles = true;
+      BlockAboutSupport = true;
+      CaptivePortal = false;
+      DisableDeveloperTools = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxAccounts = true;
+      DisableFirefoxScreenshots = true;
+      DisableFirefoxStudies = true;
+      DisableFormHistory = true;
+      DisablePocket = true;
+      DisableSetDesktopBackground = true;
+      DisableTelemetry = true;
+      DisplayBookmarksToolbar = "never";
+      DisplayMenuBar = "never";
+      DontCheckDefaultBrowser = true;
+      InstallAddonsPermission.Default = false;
+      NewTabPage = false;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      OfferToSaveLoginsDefault = false;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage =  "";
+      PasswordManagerEnabled = false;
+      Homepage = {
+        URL = "about:blank";
+        Locked = true;
+      };
+      UserMessaging = {
+        ExtensionRecommendations = false;
+        SkipOnboarding = true;
+      };
+    };
+  };
   sessionFile =
     (pkgs.writeTextDir "share/wayland-sessions/jellyfin-kiosk.desktop" ''
       [Desktop Entry]
       Name=Jellyfin
       Comment=A media platform
-      Exec=${lib.getExe pkgs.cage} -s -m last -- ${lib.getExe pkgs.firefox} -kiosk https://media.m7.rs
+      Exec=${lib.getExe pkgs.cage} -s -m last -- ${lib.getExe firefox} https://media.m7.rs
       Type=Application
     '').overrideAttrs
       (_: {
