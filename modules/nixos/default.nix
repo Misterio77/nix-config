76cc355f9a0c11c53e20b65f2a35482844356fcb
Gabriel Fontes <hi@m7.rs>
2025-09-23 10:49:00 -0300

    feat(modules/steam): add steam-gamescope-session.target to programs.steam.gamescopeSession

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -3,4 +3,6 @@
   hydra-auto-upgrade = import ./hydra-auto-upgrade.nix;
   openrgb = import ./openrgb.nix;
   nix-registry-prometheus-exporter = import ./nix-registry-prometheus-exporter.nix;
+  # Upstream me
+  steam = import ./steam.nix;
 }
