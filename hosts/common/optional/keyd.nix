d72d6cb0596de37c125f31f92708ffbb40ee8a35
Gabriel Fontes <hi@m7.rs>
2025-09-24 22:22:44 -0300

    feat(hosts/keyd): add mappings for my tv remote

--- hosts/common/optional/keyd.nix
+++ hosts/common/optional/keyd.nix
@@ -1,5 +1,27 @@
 {
   services.keyd = {
     enable = true;
+    keyboards = {
+      remoteControl = {
+        ids = [
+          "1915:1025"
+        ];
+        settings.main = {
+          # Homepage button
+          # Go to firefox homepage (alt+home)
+          homepage = "A-home";
+          # Back button
+          # Go back (e.g. firefox and steam)
+          rightmouse = "mouse1";
+          # Menu button
+          # Open quick settings on steam
+          # TODO: do something cool in firefox/media kiosk
+          compose = "C-2";
+          # Mic button
+          # Mute sound (yeah not very creative)
+          voicecommand = "mute";
+        };
+      };
+    };
   };
 }
