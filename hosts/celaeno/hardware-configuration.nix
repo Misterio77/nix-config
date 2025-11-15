c20485ddac742bdd4ac0488d6e7dd92b7cfb8b75
Gabriel Fontes <hi@m7.rs>
2025-11-15 15:32:46 -0300

    feat(hosts/celaeno): migrate to disko

--- hosts/celaeno/hardware-configuration.nix
+++ hosts/celaeno/hardware-configuration.nix
@@ -1,8 +1,11 @@
-{modulesPath, ...}: {
+{inputs, modulesPath, ...}: {
   imports = [
     ../common/optional/ephemeral-btrfs.nix
     (modulesPath + "/profiles/qemu-guest.nix")
+    inputs.disko.nixosModules.disko
   ];
+
+  nixpkgs.hostPlatform.system = "aarch64-linux";
 
   # Slows down write operations considerably
   nix.settings.auto-optimise-store = false;
@@ -26,19 +29,58 @@
     extraModprobeConfig = "options kvm nested=1";
   };
 
-  fileSystems = {
-    "/boot" = {
-      device = "/dev/disk/by-label/ESP";
-      fsType = "vfat";
+  disko.devices.disk.main = {
+    device = "/dev/vda";
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions = {
+        esp = {
+          name = "ESP";
+          size = "512M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+          };
+        };
+        root = {
+          size = "100%";
+          content = {
+            type = "btrfs";
+            postCreateHook = ''
+              MNTPOINT=$(mktemp -d)
+              mount -t btrfs "$device" "$MNTPOINT"
+              trap 'umount $MNTPOINT; rm -d $MNTPOINT' EXIT
+              btrfs subvolume snapshot -r $MNTPOINT/root $MNTPOINT/root-blank
+            '';
+            subvolumes = {
+              "/root" = {
+                mountOptions = ["compress=zstd"];
+                mountpoint = "/";
+              };
+              "/nix" = {
+                mountOptions = ["compress=zstd" "noatime"];
+                mountpoint = "/nix";
+              };
+              "/persist" = {
+                mountOptions = ["compress=zstd"];
+                mountpoint = "/persist";
+              };
+              "/swap" = {
+                mountOptions = ["compress=zstd" "noatime"];
+                mountpoint = "/swap";
+                swap.swapfile = {
+                  size = "8196M";
+                  path = "swapfile";
+                };
+              };
+            };
+          };
+        };
+      };
     };
   };
-
-  swapDevices = [
-    {
-      device = "/swap/swapfile";
-      size = 8196;
-    }
-  ];
-
-  nixpkgs.hostPlatform.system = "aarch64-linux";
+  fileSystems."/persist".neededForBoot = true;
 }
