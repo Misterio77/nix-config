97c616f18809f7fe921e9320311ca5da880c98ac
Gabriel Fontes <hi@m7.rs>
2025-11-15 12:42:23 -0300

    feat(atlas): migrate to disko

--- hosts/atlas/hardware-configuration.nix
+++ hosts/atlas/hardware-configuration.nix
@@ -1,7 +1,7 @@
-{
+{inputs, ...}: {
   imports = [
+    inputs.disko.nixosModules.disko
     ../common/optional/ephemeral-btrfs.nix
-    ../common/optional/encrypted-root.nix
   ];
 
   nixpkgs.hostPlatform.system = "x86_64-linux";
@@ -33,17 +33,67 @@
     };
   };
 
-  fileSystems = {
-    "/boot" = {
-      device = "/dev/disk/by-label/ESP";
-      fsType = "vfat";
+  disko.devices.disk.main = {
+    device = "/dev/nvme0n1";
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions = {
+        boot = {
+          size = "1M";
+          type = "EF02";
+        };
+        esp = {
+          name = "ESP";
+          size = "512M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+          };
+        };
+        luks = {
+          size = "100%";
+          content = {
+            name = "root";
+            type = "luks";
+            settings.allowDiscards = true;
+            content = {
+              type = "btrfs";
+              postCreateHook = ''
+                MNTPOINT=$(mktemp -d)
+                mount -t btrfs "$device" "$MNTPOINT"
+                trap 'umount $MNTPOINT; rm -d $MNTPOINT' EXIT
+                btrfs subvolume snapshot -r $MNTPOINT/root $MNTPOINT/root-blank
+              '';
+              subvolumes = {
+                "/root" = {
+                  mountOptions = ["compress=zstd"];
+                  mountpoint = "/";
+                };
+                "/nix" = {
+                  mountOptions = ["compress=zstd" "noatime"];
+                  mountpoint = "/nix";
+                };
+                "/persist" = {
+                  mountOptions = ["compress=zstd"];
+                  mountpoint = "/persist";
+                };
+                "/swap" = {
+                  mountOptions = ["compress=zstd" "noatime"];
+                  mountpoint = "/swap";
+                  swap.swapfile = {
+                    size = "8196M";
+                    path = "swapfile";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
     };
   };
-
-  swapDevices = [
-    {
-      device = "/swap/swapfile";
-      size = 8196;
-    }
-  ];
+  fileSystems."/persist".neededForBoot = true;
 }
--- hosts/common/optional/encrypted-root.nix
+++
@@ -1,7 +0,0 @@
-{config, ...}: let
-  hostname = config.networking.hostName;
-in {
-  boot.initrd = {
-    luks.devices."${hostname}".device = "/dev/disk/by-label/${hostname}_crypt";
-  };
-}
