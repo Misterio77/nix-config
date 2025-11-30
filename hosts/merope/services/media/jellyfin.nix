e838110804392ee139f5775f265423d49874a15e
Gabriel Fontes <hi@m7.rs>
2025-11-30 14:22:07 -0300

    fix(merope/jellyfin): fix eval error

--- hosts/merope/services/media/jellyfin.nix
+++ hosts/merope/services/media/jellyfin.nix
@@ -3,48 +3,50 @@
   # TODO: https://github.com/Sveske-Juice/declarative-jellyfin
   options.services.jellyfin.port = lib.mkOption { default = 8096; type = lib.types.port; };
 
-  services = {
-    jellyfin = {
-      enable = true;
-    };
-    nginx.virtualHosts = {
-      "media.m7.rs" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "http://localhost:${toString config.services.jellyfin.port}";
-          proxyWebsockets = true;
+  config = {
+    services = {
+      jellyfin = {
+        enable = true;
+      };
+      nginx.virtualHosts = {
+        "media.m7.rs" = {
+          forceSSL = true;
+          enableACME = true;
+          locations."/" = {
+            proxyPass = "http://localhost:${toString config.services.jellyfin.port}";
+            proxyWebsockets = true;
+          };
+        };
+        "music.m7.rs" = {
+          forceSSL = true;
+          enableACME = true;
+          locations."/".return = "302 https://media.m7.rs/web/#/music.html";
+        };
+        "music.misterio.me" = {
+          forceSSL = true;
+          enableACME = true;
+          locations."/".return = "302 https://media.m7.rs/web/#/music.html";
         };
       };
-      "music.m7.rs" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/".return = "302 https://media.m7.rs/web/#/music.html";
-      };
-      "music.misterio.me" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/".return = "302 https://media.m7.rs/web/#/music.html";
-      };
     };
-  };
 
-  # Make config readable by jellyfin group (e.g. jellysearch)
-  systemd = {
-    tmpfiles.settings.jellyfinDirs = {
-      "${config.services.jellyfin.dataDir}".d.mode = "750";
+    # Make config readable by jellyfin group (e.g. jellysearch)
+    systemd = {
+      tmpfiles.settings.jellyfinDirs = {
+        "${config.services.jellyfin.dataDir}".d.mode = "750";
+      };
+      services.jellyfin.serviceConfig.UMask = "0027";
     };
-    services.jellyfin.serviceConfig.UMask = "0027";
-  };
 
-  environment.persistence = {
-    "/persist".directories = [
-      {
-        directory = "/var/lib/jellyfin";
-        user = config.services.jellyfin.user;
-        group = config.services.jellyfin.group;
-        mode = "0700";
-      }
-    ];
+    environment.persistence = {
+      "/persist".directories = [
+        {
+          directory = "/var/lib/jellyfin";
+          user = config.services.jellyfin.user;
+          group = config.services.jellyfin.group;
+          mode = "0700";
+        }
+      ];
+    };
   };
 }
