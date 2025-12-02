adefff879fd603abbfe2d5e91837b41de318147c
Gabriel Fontes <hi@m7.rs>
2025-12-01 21:10:20 -0300

    fix(alcyone/grafana): try to fix policy error

--- hosts/alcyone/services/grafana/default.nix
+++ hosts/alcyone/services/grafana/default.nix
@@ -65,7 +65,7 @@
           policies.settings = {
             apiVersion = 1;
             policies = [{
-              receiver = "alerts-email";
+              receiver = "default";
               group_wait = "30s";
               group_interval = "5m";
               repeat_interval = "4h";
