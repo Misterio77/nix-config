fcfc78b14162b307d4e80df1ba2d3a9cdb1990f5
Gabriel Fontes <hi@m7.rs>
2026-02-20 11:55:35 -0300

    fix(kdeconnect): open kdeconnect ports on all devices

--- hosts/common/global/kdeconnect.nix
+++ hosts/common/global/kdeconnect.nix
@@ -1,5 +1,5 @@
 {
-  networking.firewall.interfaces."tailscale0" = {
+  networking.firewall = {
     allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
     allowedUDPPortRanges = [{ from = 1714; to = 1764; }];
   };
