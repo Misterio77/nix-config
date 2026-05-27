fcde547f2c28917c9550aaed4cb573c745a6a353
Gabriel Fontes <hi@m7.rs>
2026-05-27 17:46:13 -0300

    feat(cups): declarative cups config

--- hosts/common/optional/cups.nix
+++ hosts/common/optional/cups.nix
@@ -1,8 +1,32 @@
 {pkgs, ...}: {
+  services.ipp-usb.enable = true;
   services.printing = {
     enable = true;
+    stateless = true;
     drivers = [
       pkgs.epson-escpr2
+    ];
+  };
+  hardware.printers.ensurePrinters = [
+    {
+      description = "Epson L5590 (USB ESCPR2)";
+      name = "Epson_L5590_USB_ESCPR2";
+      deviceUri = "usb://EPSON/L5590%20Series?serial=584242573036313348&interface=1";
+      model = "epson-inkjet-printer-escpr2/Epson-L5590_Series-epson-escpr2-en.ppd";
+      ppdOptions = {
+        PageSize = "A4";
+        Ink = "COLOR";
+        MediaType = "PMPHOTO_HIGH"; # Premium Photo (usually what I use)
+        Brightness = "4";
+        Contrast = "6";
+        Saturation = "15";
+      };
+    }
+  ];
+  hardware.sane = {
+    enable = true;
+    extraBackends = [
+      pkgs.sane-airscan
     ];
   };
 }
