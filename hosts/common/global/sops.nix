c3436b23f634f875a6020dc6e2484b10ace72f37
Gabriel Fontes <hi@m7.rs>
2026-06-15 11:19:05 -0300

    feat: enable sopsdiffing for home secrets as well

--- hosts/common/global/sops.nix
+++ hosts/common/global/sops.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   config,
+  pkgs,
   ...
 }: let
   isEd25519 = k: k.type == "ed25519";
@@ -8,6 +9,7 @@
   keys = builtins.filter isEd25519 config.services.openssh.hostKeys;
 in {
   imports = [inputs.sops-nix.nixosModules.sops];
+  environment.systemPackages = [pkgs.sops];
 
   sops = {
     age.sshKeyPaths = map getKeyPath keys;
