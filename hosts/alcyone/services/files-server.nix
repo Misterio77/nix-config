3de34079af1d486726e2e6a48c39c7900ee49155
Gabriel Fontes <hi@m7.rs>
2026-06-16 23:37:42 -0300

    refactor(alcyone/files-server): simplify

--- hosts/alcyone/services/files-server.nix
+++ hosts/alcyone/services/files-server.nix
@@ -1,21 +1,19 @@
-{config, ...}: let
-  files = {
-    forceSSL = true;
-    enableACME = true;
-    locations."/" = {
-      root = "/srv/files";
-      extraConfig = ''
-        allow 127.0.0.1;
-        allow ::1;
-        allow ${config.services.headscale.settings.prefixes.v4};
-        allow ${config.services.headscale.settings.prefixes.v6};
-        deny all;
-      '';
-    };
-  };
-in {
+{config, ...}: {
   services.nginx.virtualHosts = {
-    "files.m7.rs" = files;
-    "f.m7.rs" = files;
+    "files.m7.rs" = {
+      forceSSL = true;
+      enableACME = true;
+      serverAliases = ["f.m7.rs"];
+      locations."/" = {
+        root = "/srv/files";
+        extraConfig = ''
+          allow 127.0.0.1;
+          allow ::1;
+          allow ${config.services.headscale.settings.prefixes.v4};
+          allow ${config.services.headscale.settings.prefixes.v6};
+          deny all;
+        '';
+      };
+    };
   };
 }
