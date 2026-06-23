36de83a5833719cc6a5607cf29c99c5d7d07315c
Gabriel Fontes <hi@m7.rs>
2026-06-23 15:32:21 -0300

    fix(pkgs/jagex-auth): remove chromium and wlclip deps
    These are option

--- pkgs/jagex-auth/default.nix
+++ pkgs/jagex-auth/default.nix
@@ -4,8 +4,6 @@
   makeWrapper,
   python3,
   stdenvNoCC,
-  ungoogled-chromium,
-  wl-clipboard,
 }: let
   python = python3.withPackages (ps: [ps.requests]);
 in
@@ -24,10 +22,6 @@ in
       install -Dm644 $src $out/share/jagex-auth/jagex-auth.py
       makeWrapper ${lib.getExe python} $out/bin/jagex-auth \
         --add-flags $out/share/jagex-auth/jagex-auth.py \
-        --prefix PATH : ${lib.makeBinPath [
-        ungoogled-chromium
-        wl-clipboard
-      ]}
 
       runHook postInstall
     '';
