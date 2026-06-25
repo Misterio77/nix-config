55a31ce1db9fc2e8e68ccef51e51c9a768d4ead6
Gabriel Fontes <hi@m7.rs>
2026-06-25 14:33:46 -0300

    feat(merorpe/immich): make public again

--- hosts/merope/services/immich.nix
+++ hosts/merope/services/immich.nix
@@ -1,4 +1,4 @@
-{config, outputs, ...}: {
+{config, ...}: {
   services.immich = {
     enable = true;
     accelerationDevices = ["/dev/dri/renderD128"];
@@ -14,13 +14,6 @@
     locations."/" = {
       proxyPass = "http://localhost:${toString config.services.immich.port}";
       proxyWebsockets = true;
-      extraConfig = ''
-        allow 127.0.0.1;
-        allow ::1;
-        allow ${outputs.nixosConfigurations.alcyone.config.services.headscale.settings.prefixes.v4};
-        allow ${outputs.nixosConfigurations.alcyone.config.services.headscale.settings.prefixes.v6};
-        deny all;
-      '';
     };
   };
 
